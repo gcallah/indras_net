e33720612f281b74786e16ea9746e3b9bd73a8b2
gcallah <gcallah@mac.com>
2015-01-11 18:50:59 -0500

    Added a basic capabilities demo for notebook.

--- basic_run.py
+++ basic_run.py
@@ -4,33 +4,37 @@ import prop_args as props
 import entity as ent
 import basic_model as bm
 
+# set up some file names:
 MODEL_NM = "basic_model"
 PROG_NM  = MODEL_NM + ".py"
 LOG_FILE = MODEL_NM + ".txt"
 
-#logging.basicConfig(format='%(levelname)s:%(message)s',
-#        level=logging.INFO, filemode='w', filename=LOG_FILE)
-
-
+# We store basic parameters in a "property" file; this allows us to save
+#  multiple parameter sets, which is important in simulation work.
+#  We can read these in from file or set them here.
 read_props = False
-
 if read_props:
     pa = props.PropArgs.read_props(MODEL_NM, "basic.props")
 else:
     pa = props.PropArgs(MODEL_NM, logfile=LOG_FILE, props=None)
     pa.set("model", MODEL_NM)
     pa.set("num_agents", 10)
-    pa.set("user_type", ent.User.IPYTHON)
+    pa.set("user_type", ent.User.IPYTHON_NB)
 
-
+# Now we create a minimal environment for our agents to act within:
 env = bm.BasicEnv(model_nm=MODEL_NM)
+# And put the environment in the properties object:
 pa.set("env", env)
 
+# Now we loop creating multiple agents with numbered names based on the loop variable:
 for i in range(pa.get("num_agents")):
     env.add_agent(
             bm.BasicAgent(name="agent" + str(i),
             goal="acting up!"))
 
+# Logging is automatically set up for the modeler:
 logging.info("Starting program " + PROG_NM)
+
+# And now we set things running!
 env.run()
 
