68da2af3dcd0d9a71605d0a34a5deaf0677853c2
gcallah <gcallah@mac.com>
2015-01-12 10:34:28 -0500

    Extended CSV files to add more goods and agents.

--- barter_run.py
+++ barter_run.py
@@ -7,7 +7,7 @@ import barter_model as bm
 MODEL_NM = "barter_model"
 PROG_NM  = MODEL_NM + ".py"
 LOG_FILE = MODEL_NM + ".txt"
-CSV_FILE = MODEL_NM + ".csv"
+CSV_FILE = MODEL_NM + "02.csv"
 PROPS_FILE = MODEL_NM + ".props"
 
 
@@ -25,19 +25,6 @@ env = bm.BarterEnv("A barter market", 50.0, 50.0,
                     model_nm=MODEL_NM)
 pa.set("env", env)
 env.fetch_agents_from_file(CSV_FILE)
-
-
-#charles = bm.BarterAgent(name="Charles")
-#env.add_agent(charles)
-#charles.endow(bm.WINE,
-#                pa.get("cha_wine"),
-#                util_func=eval("lambda qty: " + pa.get("cha_wutil")))
-#charles.endow(bm.CHEESE,
-#                pa.get("cha_cheese"),
-#                util_func=eval("lambda qty: " + pa.get("cha_cutil")))
-#charles.endow(bm.OLIVE_OIL,
-#                pa.get("cha_olive"),
-#                util_func=eval("lambda qty: " + pa.get("cha_outil")))
 
 ent.Entity.add_universal(bm.BarterAgent, bm.TRADE, bm.BarterAgent)
 
