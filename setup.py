c6a28a48e94017ea2a4aae8f0c1edcba9cf29810
gcallah <gcallah@mac.com>
2015-02-17 15:57:26 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='indras_net',
+      version='0.1',
+      description='Agent-based modeling in Python.',
+      url='http://github.com/gcallah/indra',
+      author='Gene Callahan',
+      author_email='gcallah@mac.com',
+      license='GNU',
+      packages=['indra'],
+      zip_safe=False)
+
+
