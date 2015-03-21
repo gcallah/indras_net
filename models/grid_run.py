899523aa146619c9dd02e1aa49a3678acac30580
gcallah <gcallah@mac.com>
2015-03-21 19:06:54 -0400

    grid_run.py

--- models/grid_run.py
+++ models/grid_run.py
@@ -21,9 +21,9 @@ if read_props:
 else:
     pa = props.PropArgs(MODEL_NM, logfile=LOG_FILE, props=None)
     pa.set("model", MODEL_NM)
-    pa.set("num_agents", 6)
-    pa.set("grid_width", 3)
-    pa.set("grid_height", 3)
+    pa.set("num_agents", 1)
+    pa.set("grid_width", 2)
+    pa.set("grid_height", 2)
 
 # Now we create a minimal environment for our agents to act within:
 env = ge.GridEnv("Test grid env",
