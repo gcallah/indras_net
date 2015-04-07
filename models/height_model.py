fc670ea6668ac0b18a78f0fa46607df8ee5b2a2f
blogan1 <brandon.logan@purchase.edu>
2015-04-07 13:43:56 -0400

    Trying to fix  heights

--- models/height_model.py
+++ models/height_model.py
@@ -28,10 +28,15 @@ class HeightAgent(ent.Agent):
         self.alive = False
 
     def reproduce(self):
-        self.mychild = HeightAgent(self.name + str(self.env.period), random.gauss(self.height, self.height/4))
+        #print(self.height)
+       
 
+        self.mychild = HeightAgent(self.name + str(self.env.period),
+                                   random.uniform(self.height - 1, self.height + 1))
+        #print(new_height)                           
         self.env.add_child(self.mychild)
         #print('adding to womb ')
+        
 
 class HeightAgentEng(HeightAgent):
 
@@ -40,24 +45,26 @@ class HeightAgentEng(HeightAgent):
     #def gen_height(self):
         #new_height = random.uniform(self.height-self.height/4,self.height+self.height/4)
 
-
     def reproduce(self):
-        self.mychild = HeightAgentEng(self.name + str(self.env.period), random.gauss(self.height, self.height/4))
+        self.mychild = HeightAgentEng(self.name + str(self.env.period),
+                                      random.uniform(self.height - 1, self.height + 1))
 
         self.env.add_child(self.mychild)
 
+        if self.mychild.height < self.env.runt_height:
+            self.mychild.height = self.env.runt_height
+            print('work')
 
-#       runt_height = .67 * HeightEnv.census.avg_height[var]
-#       self.mychild.height = runt_height
+
 
 class HeightEnv(env.Environment):
 
     """ This class creates an environment for Schelling height agents """
 
-
     def __init__(self, model_nm = None):
         super().__init__( "Height Environment", model_nm=model_nm, preact=True)
         self.avg_height = {}
+        self.runt_height = 0
         
     def census(self, disp=True):                                                        
         """                                                                             
@@ -66,16 +73,17 @@ class HeightEnv(env.Environment):
         self.user.tell("Average heights in period " + str(self.period))                        
         for var in self.agents.varieties_iter():                                        
             total_height = 0                                                              
-            i = 0
+            num_agents = self.get_pop(var)
                                                                             
             for agent in self.agents.variety_iter(var):                                 
-                total_height += agent.height                                               
-                i += 1
-                
-            self.avg_height[var] = total_height / i                                           
-            self.agents.append_pop_hist(var, self.avg_height)                                
+                total_height += agent.height
+              
+            self.avg_height[var] = total_height / num_agents 
+            self.agents.append_pop_hist(var, self.avg_height[var])   
+            if var == "HeightAgentEng":
+                self.runt_height = .8 * self.avg_height[var]   
+                print(self.runt_height)      
         self.user.tell(var + ": " + str(self.avg_height))
-        
                                                 
     def view_pop(self):                                                                 
         """                                                                             
@@ -87,7 +95,9 @@ class HeightEnv(env.Environment):
                                                                                     
         (period, data) = self.line_data()                                               
         self.line_graph = disp.LineGraph("Schelling's height model",                    
-                                     data, period, anim=False)                          
+                                         data, period, anim=False)
+                                   
+   
     def preact_loop(self):
         for agent in reversed(self.agents):
             if not agent.alive:
