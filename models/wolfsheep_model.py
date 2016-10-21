9f17a37a17d15552a879afdba2f1037b0d62a1b6
Nathan Conroy <nathanconroydev@gmail.com>
2016-10-21 01:27:39 -0500

    shortened license statement

--- models/wolfsheep_model.py
+++ models/wolfsheep_model.py
@@ -1,25 +1,9 @@
 """
-This file is part of Indra.
-
-Indra is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Indra is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
-
-"""
 wolfsheep_model.py
 Wolves and sheep roaming a meadow, with wolves eating sheep
 that get near them.
+
+Available under the terms of the GNU GPL
 """
 import random
 import indra.markov as markov
