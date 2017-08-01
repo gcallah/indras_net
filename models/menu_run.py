bf569a975a3d33590933c95516321c232dd1bc81
gcallah <gcallah@mac.com>
2017-08-01 00:58:41 -0400

    Modifying models to work with __main__

--- models/menu_run.py
+++ models/menu_run.py
@@ -11,24 +11,29 @@ import menu_model as mm
 
 # set up some file names:
 MODEL_NM = "menu_model"
-(prog_file, log_file, prop_file, results_file) = utils.gen_file_names(MODEL_NM)
 
-# We store menu parameters in a
-# "property" file; this allows us to save
-#  multiple parameter sets, which is important in simulation work.
-#  We can read these in from file or set them here.
-pa = utils.read_props(MODEL_NM)
-if pa is None:
-    pa = props.PropArgs(MODEL_NM, logfile=log_file, props=None)
-    pa.set("num_agents", 10)
+def run():
+    (prog_file, log_file, prop_file, results_file) = utils.gen_file_names(MODEL_NM)
+    
+    # We store menu parameters in a
+    # "property" file; this allows us to save
+    #  multiple parameter sets, which is important in simulation work.
+    #  We can read these in from file or set them here.
+    pa = utils.read_props(MODEL_NM)
+    if pa is None:
+        pa = props.PropArgs(MODEL_NM, logfile=log_file, props=None)
+        pa.set("num_agents", 10)
+    
+    # Now we create a minimal environment for our agents to act within:
+    env = mm.MenuEnv(model_nm=MODEL_NM)
+    
+    # Now we loop creating multiple agents
+    #  with numbered names based on the loop variable:
+    for i in range(pa.get("num_agents")):
+        env.add_agent(mm.MenuAgent(name="agent" + str(i),
+                                   goal="testing our menu capabilities!"))
+    
+    utils.run_model(env, prog_file, results_file)
 
-# Now we create a minimal environment for our agents to act within:
-env = mm.MenuEnv(model_nm=MODEL_NM)
-
-# Now we loop creating multiple agents
-#  with numbered names based on the loop variable:
-for i in range(pa.get("num_agents")):
-    env.add_agent(mm.MenuAgent(name="agent" + str(i),
-                               goal="testing our menu capabilities!"))
-
-utils.run_model(env, prog_file, results_file)
+if __name__ == "__main__":
+    run()
