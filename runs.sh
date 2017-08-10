7bf40084037c2222443de204bcd09cb7c47d66aa
gcallah <gcallah@mac.com>
2017-08-10 00:35:05 -0400

    Script to run model mutliple times.

---
+++ runs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+num_runs=$1
+run_file=$2
+props=$3
+data_base=$4
+periods=$5
+
+for ((i=1;i < num_runs; i++))
+do
+    datafile="$data_base.$i.csv"
+    python $run_file $props -periods $periods -datafile $datafile
+done
