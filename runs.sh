91d77694e6a31799785b6f0e8976425637fed028
gcallah <gcallah@mac.com>
2017-08-10 00:41:34 -0400

    Redirect STDOUT.

--- runs.sh
+++ runs.sh
@@ -6,8 +6,8 @@ props=$3
 data_base=$4
 periods=$5
 
-for ((i=1;i < num_runs; i++))
+for ((i=1;i <= num_runs; i++))
 do
     datafile="$data_base.$i.csv"
-    python $run_file $props -periods $periods -datafile $datafile
+    python $run_file $props -periods $periods -datafile $datafile > /dev/null
 done
