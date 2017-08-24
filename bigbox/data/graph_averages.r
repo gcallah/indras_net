d8b4e39e55c1f4e78e6d2f6ae30479f9db11c8d6
NathanConroy <nathanconroydev@gmail.com>
2017-08-23 21:22:12 -0500

    Remove working variables at the beginning.

--- bigbox/data/graph_averages.r
+++ bigbox/data/graph_averages.r
@@ -2,10 +2,10 @@
 # Graphs the average amount of bigboxes, momandpops,
 # and consumer utility in each .csv file in the mppref1.2 directory.
 
+rm(list=ls())
+
 require(ggplot2)
 require(reshape2)
-
-rm(list=ls())
 
 files <- list.files(path = "./mppref1.2", pattern ="(.+)csv")
 filesWithPaths <- unlist(lapply(files, function(y) (paste("mppref1.2/", y, sep = ""))))
