b8b5d434a1a9c52c5067a05354d3929abd724d0f
NathanConroy <nathanconroydev@gmail.com>
2017-08-23 21:19:16 -0500

    Cleaned up code.

--- bigbox/data/graph_averages.r
+++ bigbox/data/graph_averages.r
@@ -8,22 +8,15 @@ require(reshape2)
 rm(list=ls())
 
 files <- list.files(path = "./mppref1.2", pattern ="(.+)csv")
-
 filesWithPaths <- unlist(lapply(files, function(y) (paste("mppref1.2/", y, sep = ""))))
-
 dataSets <- lapply(filesWithPaths, read.csv)
 
+# Get the average of the data sets.
 average <- Reduce("+", dataSets) / length(dataSets)
 
-print(average)
-
+# Add column for x-axis.
 steps = as.numeric(rownames(average))
-
 average[,"step"] <- steps
 
-print(average)
-
-average <- melt(average, id.vars="step")
-
-ggplot(average, aes(step,value, col=variable)) + 
+ggplot(melt(average, id.vars="step"), aes(step,value, col=variable)) + 
   geom_line()
