b290b56ebbf0adbd5c851e0545834cfde2d9d15c
NathanConroy <nathanconroydev@gmail.com>
2017-08-28 22:50:22 -0500

    Moved graphing script into bigbox.

--- all_box_plots.sh
+++ all_box_plots.sh
@@ -11,16 +11,16 @@ echo $DATA_DIR
 
 OUT_DIR="$DATA_DIR/mppref1.0"
 ./runs.sh $NUM_RUNS $MODEL "$OUT_DIR/$PREFS" "$OUT_DIR/run" $PERIODS
-Rscript graph_averages.r "$OUT_DIR" "$OUT_DIR/plot_mppref1.0.pdf"
+Rscript "$BOX_DIR/graph_averages.r" "$OUT_DIR" "$OUT_DIR/plot_mppref1.0.pdf"
 OUT_DIR="$DATA_DIR/mppref1.2"
 ./runs.sh $NUM_RUNS $MODEL "$OUT_DIR/$PREFS" "$OUT_DIR/run" $PERIODS
-Rscript graph_averages.r "$OUT_DIR" "$OUT_DIR/plot_mppref1.2.pdf"
+Rscript "$BOX_DIR/graph_averages.r" "$OUT_DIR" "$OUT_DIR/plot_mppref1.2.pdf"
 OUT_DIR="$DATA_DIR/mppref1.4"
 ./runs.sh $NUM_RUNS $MODEL "$OUT_DIR/$PREFS" "$OUT_DIR/run" $PERIODS
-Rscript graph_averages.r "$OUT_DIR" "$OUT_DIR/plot_mppref1.4.pdf"
+Rscript "$BOX_DIR/graph_averages.r" "$OUT_DIR" "$OUT_DIR/plot_mppref1.4.pdf"
 OUT_DIR="$DATA_DIR/mppref1.6"
 ./runs.sh $NUM_RUNS $MODEL "$OUT_DIR/$PREFS" "$OUT_DIR/run" $PERIODS
-Rscript graph_averages.r "$OUT_DIR" "$OUT_DIR/plot_mppref1.6.pdf"
+Rscript "$BOX_DIR/graph_averages.r" "$OUT_DIR" "$OUT_DIR/plot_mppref1.6.pdf"
 OUT_DIR="$DATA_DIR/mppref1.8"
 ./runs.sh $NUM_RUNS $MODEL "$OUT_DIR/$PREFS" "$OUT_DIR/run" $PERIODS
-Rscript graph_averages.r "$OUT_DIR" "$OUT_DIR/plot_mppref1.8.pdf"
+Rscript "$BOX_DIR/graph_averages.r" "$OUT_DIR" "$OUT_DIR/plot_mppref1.8.pdf"
