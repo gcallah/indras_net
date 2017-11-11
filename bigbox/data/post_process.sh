6d9b9c85eb60c5929acbf24418083a671d56f183
NathanConroy <nathanconroydev@gmail.com>
2017-11-10 23:01:38 -0600

    post process data

---
+++ bigbox/data/post_process.sh
@@ -0,0 +1,7 @@
+
+# Rscript post_process.r data_dir output_dir
+Rscript post_process.r "mppref1.0" "mppref1.0"
+Rscript post_process.r "mppref1.2" "mppref1.2"
+Rscript post_process.r "mppref1.4" "mppref1.4"
+Rscript post_process.r "mppref1.6" "mppref1.6"
+Rscript post_process.r "mppref1.8" "mppref1.8"
