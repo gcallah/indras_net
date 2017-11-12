b91556951df266932ecbbad6fdf48e04a07a6165
NathanConroy <nathanconroydev@gmail.com>
2017-11-11 23:23:11 -0600

    processing the summary text file so we can use awk to get average

--- bigbox/data/post_process.sh
+++ bigbox/data/post_process.sh
@@ -1,7 +1,31 @@
 
 # Rscript post_process.r data_dir output_dir
-Rscript post_process.r "mppref1.0" "mppref1.0"
-Rscript post_process.r "mppref1.2" "mppref1.2"
-Rscript post_process.r "mppref1.4" "mppref1.4"
-Rscript post_process.r "mppref1.6" "mppref1.6"
-Rscript post_process.r "mppref1.8" "mppref1.8"
+summary_file_extension="_summary.txt"
+
+data_dir="mppref1.0"
+output_dir="mppref1.0"
+Rscript post_process.r $data_dir $output_dir;
+vim -c ":%s/://g" -c "wq" "$data_dir/$output_dir$summary_file_extension";
+
+data_dir="mppref1.2"
+output_dir="mppref1.2"
+Rscript post_process.r $data_dir $output_dir;
+vim -c ":%s/://g" -c "wq" "$data_dir/$output_dir$summary_file_extension";
+
+data_dir="mppref1.4"
+output_dir="mppref1.4"
+Rscript post_process.r $data_dir $output_dir;
+vim -c ":%s/://g" -c "wq" "$data_dir/$output_dir$summary_file_extension";
+
+data_dir="mppref1.6"
+output_dir="mppref1.6"
+Rscript post_process.r $data_dir $output_dir;
+vim -c ":%s/://g" -c "wq" "$data_dir/$output_dir$summary_file_extension";
+
+data_dir="mppref1.8"
+output_dir="mppref1.8"
+Rscript post_process.r $data_dir $output_dir;
+vim -c ":%s/://g" -c "wq" "$data_dir/$output_dir$summary_file_extension";
+
+# use awk script to process data
+#awk '/Min/ { sum += $2; n++ } END { if (n > 0) print (sum / n); }' "mppref1.0/mppref1.0__summary.txt" > "trial.txt";
