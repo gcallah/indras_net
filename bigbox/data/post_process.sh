00699bf47e6dbc7fde81e6069e6fc03391a99a6f
NathanConroy <nathanconroydev@gmail.com>
2017-11-11 23:42:21 -0600

    added awk-calculated averages

--- bigbox/data/post_process.sh
+++ bigbox/data/post_process.sh
@@ -7,6 +7,11 @@ data_dir="mppref1.0"
 output_dir="mppref1.0"
 Rscript post_process.r $data_dir $output_dir;
 vim -c ":%s/://g" -c ":%s/Qu.//g" -c "wq" "$data_dir/$output_dir$summary_file_extension";
+awk '/Min/ { sum += $2; n++ } END { if (n > 0) print (sum / n); }' "$data_dir/$output_dir$summary_file_extension" > "trial.txt";
+awk '/1st/ { sum += $2; n++ } END { if (n > 0) print (sum / n); }' "$data_dir/$output_dir$summary_file_extension" >> "trial.txt";
+awk '/Median/ { sum += $2; n++ } END { if (n > 0) print (sum / n); }' "$data_dir/$output_dir$summary_file_extension" >> "trial.txt";
+awk '/3rd/ { sum += $2; n++ } END { if (n > 0) print (sum / n); }' "$data_dir/$output_dir$summary_file_extension" >> "trial.txt";
+awk '/Max/ { sum += $2; n++ } END { if (n > 0) print (sum / n); }' "$data_dir/$output_dir$summary_file_extension" >> "trial.txt";
 
 data_dir="mppref1.2"
 output_dir="mppref1.2"
@@ -29,4 +34,3 @@ Rscript post_process.r $data_dir $output_dir;
 vim -c ":%s/://g" -c ":%s/Qu.//g" -c "wq" "$data_dir/$output_dir$summary_file_extension";
 
 # use awk script to process data
-#awk '/Min/ { sum += $2; n++ } END { if (n > 0) print (sum / n); }' "mppref1.0/mppref1.0__summary.txt" > "trial.txt";
