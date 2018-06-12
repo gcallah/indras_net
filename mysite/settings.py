d6faacaf43ce551f3560def826f3342647097fc5
zyyggdk <674977156@qq.com>
2018-06-12 11:47:07 -0400

    Fixed bugs that logo and stylesheet can be loaded

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "mysite", "static"), ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
