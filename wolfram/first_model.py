e333f13dee6243527fff26cc15ad9785f3b4eb70
zyyggdk <674977156@qq.com>
2018-06-12 11:56:14 -0400

    Revert "add run button"
    This reverts commit 91d9a26c0e7bbdfd2dd2723ce2045abe1ae8fd74.

--- wolfram/first_model.py
+++ wolfram/first_model.py
@@ -7,19 +7,13 @@ new features added to the base system.
 """
 import indra.grid_agent as ga
 
-black = 0
-white = 1
 
 class TestGridAgent(ga.GridAgent):
     """
     An agent that prints its neighbors in preact
-    and also jumps to an empty cell: default behavior
+    and also jumps to an empty cell: defaut behavior
     from our ancestor.
     """
-    def __init__(self, name, goal, max_move=1, max_detect=1, cell=None):
-        super().__init__(name, goal, max_move=1, max_detect=1, cell=None)
-        self.state = None
-        self.next_state = None
 
     def preact(self):
         (x, y) = self.pos
@@ -31,16 +25,6 @@ class TestGridAgent(ga.GridAgent):
         for neighbor in self.neighbor_iter():
             (x1, y1) = neighbor.pos
             print("    %i, %i" % (x1, y1))
-            if
-
-    def set_color(self, new_color):
-        old_type = self.state
-        self.state = new_color
-        self.env.change_agent_type(self, old_type, self.ntype)
 
     def postact(self):
         print("Agent %s postacting" % (self.name))
-        if self.next_state is not None and self.next_state != self.state:
-            # print("Setting state to " + str(self.next_state))
-            self.set_state(self.next_state)
-            self.next_state = None
