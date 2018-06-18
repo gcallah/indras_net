06ad38a3e8a1af30f6cfb16c6d0d83a3bfd65fc8
gcallah <gcallah@mac.com>
2018-06-18 17:48:24 -0400

    Added some enhancements to indra_container.sh and put a shell file in the container.

--- indra_container.sh
+++ indra_container.sh
@@ -4,4 +4,5 @@ if [ -z "$1" ]
     echo "You must provide the location of your Indra repo."
     exit 1
 fi
-docker run -it -p 8000:8000 -v $1:/home/IndrasNet --name indra gcallah/indra:v2 bash
+docker rm indra || true
+docker run -it -p 8000:8000 -v $1:/home/IndrasNet --name indra gcallah/indra:v6 bash
