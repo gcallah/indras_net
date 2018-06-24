779fd68f7652abb75ee2c14ef2445c47f2e42145
NathanConroy <nathanconroydev@gmail.com>
2018-06-24 00:37:44 -0500

    get property information from the DB; working for basic_model

--- models/basic_run.py
+++ models/basic_run.py
@@ -28,7 +28,6 @@ def run(prop_dict=None):
         pa = utils.read_props(MODEL_NM)
     else:
         pa = props.PropArgs(MODEL_NM, logfile=log_file, props=None)
-        utils.get_agent_num(pa, "num_agents", "agents", 16)
 
     # Now we create a minimal environment for our agents to act within:
     env = bm.BasicEnv(model_nm=MODEL_NM, props=pa)
