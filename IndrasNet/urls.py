ecdd6ce44307dc02257a493e9f5f230608ac7a35
zyyggdk <674977156@qq.com>
2018-06-25 14:31:23 -0400

    Fix urls.py

--- IndrasNet/urls.py
+++ IndrasNet/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
     url(r'^run/*$', views.run, name='run'),
     url(r'^help/*$', views.help, name='help'),
     url(r'^feedback/*$', views.feedback, name='feedback'),
-    url(r'^about/*$', views.about, name='about'),
-    url(r'^plot/*$', views.plot, name='plot')
+    url(r'^about/*$', views.about, name='about')
 ]
