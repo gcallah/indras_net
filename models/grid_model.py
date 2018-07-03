dcc139a794899231c3a0c08036faca53129dba32
zyyggdk <674977156@qq.com>
2018-07-03 15:19:01 -0400

    Tried again by commenting out prints

--- models/grid_model.py
+++ models/grid_model.py
@@ -18,14 +18,12 @@ class TestGridAgent(ga.GridAgent):
 
     def preact(self):
         (x, y) = self.pos
-        u.tell("With " + self.name
-              + " we are looking around "
-              + " x = " + str(x)
-              + " y = " + str(y))
-        u.tell(self.name + " has neighbors: ")
+        #u.tell("With " + self.name + " we are looking around " + " x = " + str(x) + " y = " + str(y))
+        #u.tell(self.name + " has neighbors: ")
         for neighbor in self.neighbor_iter():
             (x1, y1) = neighbor.pos
-            u.tell("    %i, %i" % (x1, y1))
+            #u.tell("    %i, %i" % (x1, y1))
 
     def postact(self):
-        u.tell("Agent %s postacting" % (self.name))
+        pass
+        #u.tell("Agent %s postacting" % (self.name))
