2d903bc3851e73f78cd359c83d6562e0f52895a0
gcallah <gcallah@mac.com>
2018-07-03 15:52:40 -0400

    Added user_type env var.

--- inside_container.sh
+++ inside_container.sh
@@ -4,4 +4,6 @@
 # Although *inside* the container the web server runs at 0.0.0.0:8000,
 # from outside it appears at 127.0.0.1:8000.
 
+EXPORT user_type="Web browser"
+
 python ./manage.py runserver 0.0.0.0:8000
