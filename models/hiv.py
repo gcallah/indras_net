c4f5d7b61885a15bce25a669c00e2a385e28bfc0
Will-Zhiwei <zy1129@nyu.edu>
2018-09-22 22:43:39 -0400

    hiv model parameters

--- models/hiv.py
+++ models/hiv.py
@@ -1,7 +1,25 @@
-import indea.markov as markov
+import indra.markov as markov
 import indra.markov_agent as ma
 import indra.markov_env as me
 
-class people(ma.MarkovAgent):
+class Person(ma.MarkovAgent):
+    """
+    A person wanders around the environment when not coupled.
+    
+    Attributes:
+        infected: true if a person is infected with HIV
+        known: true if the infection is known (and the infected must also be true)
+        infection_length: how long the person has been infected
+        coupled: true if sexually coupled
+        coupled_length: how long the person has been coupled
+        commitment: how long the person will stay coupled
+        coupling_tendency: how likely the person is to be coupled
+        condom_use: the percent chance the person uses protection
+        test_frequence: number of times the person gets tested per year
+        partner: with whom the person is coupled with
+    """
+
+    def __init__(self, infected, known, infection_length, coupled, coupled_length, commitment, coupling_tendency, condom_use, test_frequence, partner):
+
 
 
---
+++ models/hiv_run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+'''
+average_coupling_tendency
+average_commitment
+average_condom_use
+average_test_frequency
+'''
+
