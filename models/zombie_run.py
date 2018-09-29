4c5628487d4df8155d072ca4fd9655c08e714da8
JacEkko <jgk305@nyu.edu>
2018-09-28 21:22:22 -0400

    updated zombie_run.py to work with the code we created in zombie.py

--- models/zombie_run.py
+++ models/zombie_run.py
@@ -1,50 +1,45 @@
 #!/usr/bin/env python3
 """
-A script to test our grid capabilities.
+A predator-prey model with wolves and sheep.
 """
-MODEL_NM = "grid"
+MODEL_NM = "zombie"
 
 import indra.prop_args2 as props
 pa = props.PropArgs.create_props(MODEL_NM)
 
 import indra.utils as utils
-import indra.grid_env as ge
-import models.grid as gm
-import logging
-import os
+import indra.prop_args as props
+import models.wolfsheep as wsm
 
+# set up some file names:
 
 def run(prop_dict=None):
     (prog_file, log_file, prop_file, results_file) = utils.gen_file_names(MODEL_NM)
     
     global pa
-    
-    if pa["user_type"] == props.WEB:
-        pa["base_dir"] = os.environ["base_dir"]
-    
-    # Now we create a minimal environment for our agents to act within:
-    env = ge.GridEnv("Test grid env",
+
+    # we create a meadow for our agents to act within:
+    env = wsm.Meadow("Infected Zone",
                      pa["grid_width"],
                      pa["grid_height"],
-                     torus=False,
                      model_nm=MODEL_NM,
                      preact=True,
                      postact=True,
                      props=pa)
     
     # Now we loop creating multiple agents with numbered names
-    # based on the loop variable:
-    for i in range(pa["num_agents"]):
-        env.add_agent(gm.TestGridAgent(name="agent" + str(i),
-                      goal="taking up a grid space!"))
+    # based on the number of agents of that type to create:
+    for i in range(pa["num_zombies"]):
+        env.add_agent(wsm.Wolf("Zombie" + str(i), "Eating Human",
+                               pa["zombie_repro"],
+                               pa["zombie_lforce"],
+                               rand_age=True))
+    for i in range(pa["num_humans"]):
+        env.add_agent(wsm.Sheep("Human" + str(i), "Reproducing",
+                                pa["human_repro"],
+                                pa["human_lforce"],
+                                rand_age=True))
     
-    # let's test our iterator
-    for cell in env:
-        (x, y) = cell.coords
-        logging.info("Contents of cell x = " + str(x)
-              + " and y = " + str(y)
-              + " is " + str(cell.contents))
-        
     return utils.run_model(env, prog_file, results_file)
 
 if __name__ == "__main__":
