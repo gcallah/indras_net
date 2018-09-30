9248299a7e82d7a154f41d8837a6422262a1209f
Will-Zhiwei <zy1129@nyu.edu>
2018-09-29 22:14:03 -0400

    HIV pre- and post- act methods added

--- models/hiv.py
+++ models/hiv.py
@@ -1,25 +1,66 @@
-import indra.markov as markov
+
+import random
 import indra.markov_agent as ma
-import indra.markov_env as me
+import indra.markov_env as menv
 
-class Person(ma.MarkovAgent):
+NSTATES = 4
+
+avg_coupling_tendency = 5 # 0-10
+avg_commitment = 20 # 0-200
+avg_condom_use = 5 # 0-10
+avg_test_frequency = 1 # 0-2
+
+infection_chance = 50 # 0-100
+symptoms_show = 200
+
+class neg(ma,MarkovAgent):
     """
-    A person wanders around the environment when not coupled.
+        An HIV-negative individual
+    """
+    def __init__(self, name, goal, init_state, max_detect=1, coupling_tendency, commitment, condom_use, test_frequency, coupled, couple_length, partner):
+        super().__init__(name, goal, NSTATES, init_state, max_detect=max_detect)
+        
+        self.coupling_tendency = avg_coupling_tendency
+        self.commitment = avg_commitment
+        self.condom_use = avg_condom_use
+        self.test_frequency = avg_test_frequency
+
+        self.coupled = False
+        self.coupled_length = 0
+        self.partner = None
     
-    Attributes:
-        infected: true if a person is infected with HIV
-        known: true if the infection is known (and the infected must also be true)
-        infection_length: how long the person has been infected
-        coupled: true if sexually coupled
-        coupled_length: how long the person has been coupled
-        commitment: how long the person will stay coupled
-        coupling_tendency: how likely the person is to be coupled
-        condom_use: the percent chance the person uses protection
-        test_frequence: number of times the person gets tested per year
-        partner: with whom the person is coupled with
+    def preact(self):
+        if self.coupled = True:
+            self.coupled_length++
+
+    def act(self):
+        if self.coupled = False:
+            super().act()
+            self.state = self.next_state
+            self.move(self.state)
+        else:
+            # uncouple
+
+    def move(self, state):
+        
+
+
+class poz(neg):
     """
+        An HIV-positive individual
+    """
+    def __init__(self, name, goal, init_state, max_detect=1, coupling_tendency, commitment, condom_use, test_frequency, coupled, couple_length, partner, status_known, infection_length):
+        super().__init__(name, goal, init_state, max_detect=1, SPEED, coupling_tendency, commitment, condom_use, test_frequency, coupled, couple_length, partner)
+        status_known = False
+        infection_length = random.randint(0, symptoms_show - 1)
 
-    def __init__(self, infected, known, infection_length, coupled, coupled_length, commitment, coupling_tendency, condom_use, test_frequence, partner):
+    def preact(self):
+        self.infection_length++
 
-
+class People(menv.MarkovEnv):
+    """
+        Individuals wander around randomly when they are not in couples. Upon coming into contact with a suitable partner, there is a chance the two individuals will couple together. When this happens, the two individuals no longer move around, they stand next to each other holding hands as a representation of two people in a sexual relationship.
+    """
+    def get_pre(self, agent)
+        trans_str = ""
 
