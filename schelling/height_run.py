a8d720c81299c01f453ac8cb519530e4e82da5ff
NathanConroy <nathanconroydev@gmail.com>
2018-10-14 19:13:28 -0500

    Merge branch 'master' of https://github.com/gcallah/indras_net + Work on making Height Model serializable + Made Height Model work on prop_args2

--- schelling/height_run.py
+++ schelling/height_run.py
@@ -5,22 +5,18 @@ Created on Thu Sep 18 16:45:50 2014
 
 @author: Gene Callahan and Brandon Logan
 """
-MODEL_NM = "height"
-
 import indra.prop_args2 as props
-pa = props.PropArgs.create_props(MODEL_NM)
 
-import indra.utils as utils
-import indra.prop_args as props
-import schelling.height as hm
+MODEL_NM = "height"
 
 START_HEIGHT = 100.0
 
 
 def run(prop_dict=None):
+    pa = props.PropArgs.create_props(MODEL_NM, prop_dict)
+    import indra.utils as utils
+    import schelling.height as hm
     (prog_file, log_file, prop_file, results_file) = utils.gen_file_names(MODEL_NM)
-
-    global pa
 
     env = hm.HeightEnv(model_nm=MODEL_NM, props=pa)
     for i in range(pa["num_agents"]):
@@ -31,6 +27,7 @@ def run(prop_dict=None):
                 hm.HeightAgent('agent' + str(i), START_HEIGHT, START_HEIGHT))
     
     return utils.run_model(env, prog_file, results_file)
+
 
 if __name__ == "__main__":
     run()
