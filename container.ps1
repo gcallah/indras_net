6909ee9a921bccd049457c257ddef191b0633f6b
Sonny Arrrr <spr332@nyu.edu>
2018-11-28 23:15:39 -0500

    Added powershell env parameter for Docker
    Docker is usually done in cmd.exe and needed this.

--- container.ps1
+++ container.ps1
@@ -1,3 +1,7 @@
 # Windows Powershell Script for Indra Docker Container
-docker rm indra | true
+#Tell docker that we're using powershell
+docker-machine env --shell=powershell | Invoke-Expression
+#remove any still open indra containers
+docker rm indra -f
+#open container
 docker run -it -p 8000:8000 -v ${PWD}:/home/IndrasNet indra bash
