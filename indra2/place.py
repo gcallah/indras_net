9b09434657e2b84c5b85379051cef7bfbefe107c
gcallah <gcallah@mac.com>
2019-01-23 02:19:05 -0500

    Starting to work out what a place is.

--- indra2/place.py
+++ indra2/place.py
@@ -1,23 +1,25 @@
 """
 This file defines Place, which is a collection
-of entities that share a place. A boolean flag may make this
-a Place exlusive to a single entity.
+of entities that share a place.
 """
-from entity import def_coords
 from composite import Composite
+
+
+class SpatialRelation():
+    def __init__(self, distance, theta):
+        self.distance = distance
+        self.theta = theta
 
 
 class Place(Composite):
     """
     A collection of entities that share a location.
-    The 'atomic' param to the constructor will indicate
-    whether only one entity can occupy this place.
-    We will make atomic=False the default, since it
-    is the atomic=True case that will require over-riding
-    many of the Composite class's methods.
+    The location is related to others by a distance
+    and an angle.
+    A set of places is essentially a (distance) weighted
+    graph, but one with a built-in geometrical representation,
+    due to theta.
     """
 
-    def __init__(self, name, attrs=None, members=None,
-                 coords=def_coords, atomic=False):
+    def __init__(self, name, attrs=None, members=None):
         super().__init__(name, attrs=attrs, members=members)
-        self.coords = coords
