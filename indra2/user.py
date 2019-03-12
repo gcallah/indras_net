5b28c072aa9d267b07ad746186d52f195dea1184
zyyggdk <674977156@qq.com>
2019-03-12 07:21:36 -0400

    Change the use of words

--- indra2/user.py
+++ indra2/user.py
@@ -42,7 +42,7 @@ RUN = 1
 
 term_menu = {RUN: (str(RUN) + ") Run for N periods (DEFAULT).", run),
              2: ("2) Display the population graph.", not_impl),
-             3: ("3) Display the scatter plot.", scatter_plot),
+             3: ("3) Display the plot.", plot),
              4: ("4) Leave menu for interactive python session.", not_impl),
              QUIT: (str(QUIT) + ") Quit.", leave)}
 
