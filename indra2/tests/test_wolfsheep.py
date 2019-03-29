a044c93d91e2c1255e4fc68518a55ef9f991beb1
shruthitandel <shruthitandel@gmail.com>
2019-03-29 09:55:00 -0700

    Added string literal constants

--- indra2/tests/test_wolfsheep.py
+++ indra2/tests/test_wolfsheep.py
@@ -5,10 +5,13 @@ This is the test suite for space.py.
 from unittest import TestCase, main
 from indra2.agent import Agent
 from indra2.wolfsheep import create_sheep, create_wolf, set_up, wolf_action, sheep_action
+from indra2.wolfsheep import AGT_WOLF_NAME, AGT_SHEEP_NAME, ERR_MSG, WOLF_LIFESPAN, SHEEP_LIFESPAN
 import indra2.wolfsheep as wolfsheep
 
 TEST_SNUM = 3
 TEST_WNUM = 3
+TEST_WNAME = AGT_WOLF_NAME + str(TEST_WNUM)
+TEST_SNAME = AGT_SHEEP_NAME + str(TEST_SNUM)
 
 
 class WolfsheepTestCase(TestCase):
@@ -23,29 +26,31 @@ class WolfsheepTestCase(TestCase):
 
     def test_create_wolf(self):
         new_wolf = create_wolf(1)
-        self.assertEqual(new_wolf.name, "wolf" + str(1))
+        self.assertEqual(new_wolf.name, AGT_WOLF_NAME + str(1))
 
     def test_create_sheep(self):
         new_sheep = create_sheep(1)
-        self.assertEqual(new_sheep.name,"sheep" + str(1))
+        self.assertEqual(new_sheep.name, AGT_SHEEP_NAME + str(1))
 
     def test_wolf_action(self):
+        # yet to be enhanced
         wolf_action(self.wolf)
-        self.assertEqual(self.wolf.name, "wolf3")
-        self.assertEqual(self.wolf.duration, 13)
+        self.assertEqual(self.wolf.name, TEST_WNAME)
+        self.assertEqual(self.wolf.duration, WOLF_LIFESPAN+SHEEP_LIFESPAN)  # because wolf eats sheep
 
     def test_wolf_action_neg_case(self):
         test_neg_case = wolf_action(self.sheep)
-        self.assertEqual(test_neg_case, "Invalid agent name")
+        self.assertEqual(test_neg_case, ERR_MSG)
 
     def test_sheep_action(self):
+        # yet to be enhanced
         sheep_action(self.sheep)
-        self.assertEqual(self.sheep.name, "sheep3")
-        self.assertEqual(self.sheep.duration, 8)
+        self.assertEqual(self.sheep.name, TEST_SNAME)
+        self.assertEqual(self.sheep.duration, SHEEP_LIFESPAN)
 
     def test_sheep_action_neg_case(self):
         test_negCase = sheep_action(self.wolf)
-        self.assertEqual(test_negCase, "Invalid agent name")
+        self.assertEqual(test_negCase, ERR_MSG)
 
 
 
