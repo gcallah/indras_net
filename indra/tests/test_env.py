87a0e10f94f8eedbb52b99e0f7ca89fcbb85d181
Karen Zhou <zz2202@nyu.edu>
2019-04-07 11:11:32 -0400

    added test_has_disp and test_headless

--- indra/tests/test_env.py
+++ indra/tests/test_env.py
@@ -4,14 +4,19 @@ This is the test suite for env.py.
 
 from unittest import TestCase, main
 
+import indra.display_methods as disp
+
 from indra.env import Env, PopHist, POP_HIST_HDR, POP_SEP
-from indra.user import TEST
+from indra.user import TEST, WEB
 from indra.agent import Agent
 from indra.tests.test_agent import create_newton
 from indra.tests.test_composite import create_calcguys, create_cambguys
 
 GRP1 = "Group1"
 GRP2 = "Group2"
+
+X = 0
+Y = 1
 
 
 class EnvTestCase(TestCase):
@@ -58,8 +63,34 @@ class EnvTestCase(TestCase):
 
     def test_add_switch(self):
         self.env.add_switch(self.newton, self.calcs, self.cambs)
-        self.assertIn((self.newton, self.calcs, self.cambs),
-                      self.env.switches)
+        self.assertIn((self.newton, self.calcs, self.cambs),self.env.switches)
+
+    def test_has_disp(self):
+        if not disp.plt_present:
+            self.assertTrue(not self.env.has_disp())
+        else:
+            self.assertTrue(self.env.has_disp())
+
+    # def test_line_data(self):
+    #     self.fill_pop_hist()
+    #     ret = self.env.line_data()
+    #     self.assertEqual(ret, (1, {GRP1: {"data": [10, 20]}, GRP2: {"data": [10, 20]}}))
+    #
+    # def test_plot_data(self):
+    #     self.pop_hist.record_pop(GRP1, 10)
+    #     ret = self.env.plot_data()
+    #     current_var = self.env.members[GRP1]
+    #     current_agent_pos = current_var[GRP1].pos
+    #     (x, y) = current_agent_pos
+    #     self.assertEqual(ret, {GRP1: {X: [x], Y: [y]}})
+
+    def test_headless(self):
+        bool = (self.env.user_type == WEB) or (self.env.user_type == TEST)
+        if bool:
+            self.assertTrue(self.env.headless())
+        else:
+            self.assertTrue(not self.env.headless())
+
 
 if __name__ == '__main__':
     main()
