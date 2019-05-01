c7dd80ec3cb4fe862ad0fa99d93876084b31ae03
Monjur Hasan <monjur.nishad@gmail.com>
2019-05-01 14:09:24 -0400

    created sandpile test file

---
+++ models/tests/test_sandpile.py
@@ -0,0 +1,12 @@
+""" from unittest import TestCase, main
+from indra.agent import Agent
+from indra.composite import Composite
+from indra.env import Env
+from models.sandpile import set_up, create_agent
+from models.sandpile import env_unfavorable, agent_action
+
+TEST_ANUM = 999999
+
+REP_RAND_TESTS = 20
+
+SMALL_GRID = 4 """
