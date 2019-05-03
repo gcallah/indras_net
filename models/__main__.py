265d62c79e2c187068e5579a49047ace6d691602
gcallah <gcallah@mac.com>
2019-05-03 10:48:27 -0400

    Added __main__ with model list.

--- models/__main__.py
+++ models/__main__.py
@@ -1,14 +1,14 @@
 from indra.entry_point import entry_point
-import models.fashion_run
-import models.forestfire_run
-import models.sand_run
-import models.wolfsheep_run
+import models.fashion
+import models.forestfire
+import models.sand
+import models.wolfsheep
 
 models = {
-         1: ("Adam Smith's Fashion Model", models.fashion_run.run),
-         2: ("Forest Fire", models.forestfire_run.run),
-         3: ("Abelian Sandpile", models.sand_run.run),
-         4: ("Wolf Sheep", models.wolfsheep_run.run)
+             1: ("Adam Smith's Fashion Model", models.fashion.main),
+             2: ("Forest Fire", models.forestfire.main),
+             3: ("Abelian Sandpile", models.sandpile.main),
+             4: ("Schelling's Segregation Model", models.segregation.main),
          }
 
 
