c4be69a6ff7c2dd97556c24a9d95ef7eecd74d6c
Yiyang Zeng <yz3622@nyu.edu>
2019-05-06 20:27:39 -0700

    Configure flask for local development

--- IndrasNetFlask/flask_app.py
+++ IndrasNetFlask/flask_app.py
@@ -5,7 +5,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello from Flask in the git repo!'
 
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
