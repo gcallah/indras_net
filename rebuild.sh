2ffe213399994933acd0d33b97fdf4436a14ecda
gcallah <gcallah@mac.com>
2019-05-21 12:54:14 -0400

    Added comment on installing packages.

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# for the dev server: fetches new code and restarts the server.
+# for the production server: fetches new code,
+# installs needed packages, and restarts the server.
 
 git pull origin master
 pip install -r docker/requirements.txt
