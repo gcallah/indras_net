c75281f917a98bc94960ea73adf1f2baf37786e7
gcallah <gcallah@mac.com>
2019-05-21 12:50:48 -0400

    Install all requirements on PythonAnywhere on reboot.

--- rebuild.sh
+++ rebuild.sh
@@ -2,5 +2,6 @@
 # for the dev server: fetches new code and restarts the server.
 
 git pull origin master
+pip install -r docker/requirements.txt
 echo "going to reboot the webserver"
 touch /var/www/indrasnet_pythonanywhere_com_wsgi.py
