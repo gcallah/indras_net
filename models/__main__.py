6a7766fe80069657fcb4d6e16094c359f496671d
gcallah <gcallah@mac.com>
2019-06-05 17:44:54 -0400

    Main model menu now working.

--- models/__main__.py
+++ models/__main__.py
@@ -7,18 +7,25 @@ import models.wolfsheep
 
 
 def main(args=None):
-    models = None
-    models_db = None
+    welcome = "Welcome to Indra! Please choose a model:"
+    stars = "*" * len(welcome)
+    model_db = None
+    model_list = None
     with open("models.json", 'r') as f:
-        models_db = json.load(f)
-    models = models_db["models_database"]
-    print("Welcome to Indra! Please choose a model:")
-    for choice, model in enumerate(models):
-        print(choice, ". ", model["name"])
+        model_db = json.load(f)
+    model_list = model_db["models_database"]
     while True:
+        print("\n",
+              stars, "\n",
+              welcome, "\n",
+              stars)
+        for choice, model in enumerate(model_list):
+            print(str(choice) + ". ", model["name"])
         choice = int(input())
-        if choice > 0 and choice < len(models):
-            print(models[choice]["run"])
+        if choice >= 0 and choice < len(model_list):
+            exec(model_list[choice]["run"])
+        else:
+            return 0
 
 if __name__ == "__main__":
     main()
