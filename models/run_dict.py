6c581300a9e0a6edefde78b83ef6adde83f8c104
gcallah <gcallah@mac.com>
2019-06-09 18:46:17 -0400

    Building production.

---
+++ models/run_dict.py
@@ -0,0 +1,16 @@
+from basic import main as bamain
+from fashion import main as famain
+from forestfire import main as ffmain
+from sandpile import main as spmain
+from segregation import main as semain
+from wolfsheep import main as wsmain
+
+
+rdict = {
+    "basic": bamain,
+    "fashion": famain,
+    "forestfire": ffmain,
+    "sandpile": spmain,
+    "segregation": semain,
+    "wolfsheep": wsmain,
+}
