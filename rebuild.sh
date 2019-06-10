26eeb37236da58540e17aaa28c276cab55d4f3ce
gcallah <gcallah@mac.com>
2019-06-10 13:47:22 -0400

    Building production.

--- rebuild.sh
+++ rebuild.sh
@@ -10,3 +10,4 @@ source /home/indrasnet/.virtualenvs/django2/bin/activate
 pip install -r docker/requirements.txt
 echo "Going to reboot the webserver"
 API_TOKEN=14ca851554fc716c30e031b0583fb7e64e05e0db pa_reload_webapp.py indrasnet.pythonanywhere.com
+touch reboot
