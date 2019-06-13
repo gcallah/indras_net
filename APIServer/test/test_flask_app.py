db6051f804875c01b963697a4694b774617e612f
Denys Fenchenko <denisfench@gmail.com>
2019-06-13 11:03:39 -0400

    Naming changes

--- APIServer/test/test_flask_app.py
+++ APIServer/test/test_flask_app.py
@@ -4,28 +4,28 @@ from flask_restplus import Resource, Api, fields
 import json
 
 
-class HelloWorldTest(TestCase):
+class hello_wold_test(TestCase):
     def setUp(self):
         self.HelloWorld = HelloWorld(Resource)
         self.Model = Models(Resource)
         self.Props = Props(Resource)
         self.Menu = Menu(Resource)
 
-    def test_HelloWorld(self):
+    def test_hello_world(self):
         """
         See if HelloWorld works.
         """
         rv = self.HelloWorld.get()
         self.assertEqual(rv, {'hello': 'world'})
 
-    def test_Get_Model(self):
+    def test_get_model(self):
         """
         See if we can get models.
         """
         rv = self.Model.get()
         self.assertEqual(type(rv), list)
 
-    def test_Get_Props(self):
+    def test_get_props(self):
         """
         See if we can get props.
         """
@@ -33,7 +33,15 @@ class HelloWorldTest(TestCase):
         rv = self.Props.get(model_id)
         self.assertEqual(type(rv), dict)
         
-    def test_Get_Menu(self):
+    def test_put_props(self):
+        """
+        Test whether we are able to put props
+        """
+        menuitem_id = 1 
+        rv = self.Props.put(menuitem_id)
+        self.assertEqual(type(rv), String)
+        
+    def test_get_menu(self):
         """
         Testing whether we are getting the menu.
         """
@@ -41,13 +49,13 @@ class HelloWorldTest(TestCase):
         rv = self.Menu.get(model_id)
         self.assertEqual(type(rv), list)
         
-    def test_Put_Menu(self):
+    def test_put_menu(self):
         """
         Testing whether we are able to put the menu in
         """
         menuitem_id = 1
         rv = self.Menu.put(menuitem_id)
-        self.assertEqual(rv, {"execute menu item": menuitem_id, "Menu": "menu will be returned here"})
+        self.assertEqual(type(rv), dict)
         
 
     # def test_Put_Props(self):
