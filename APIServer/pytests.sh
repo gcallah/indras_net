8457a54aeb4107413315b52ad1108376d86b608f
gcallah <gcallah@mac.com>
2019-06-13 09:09:31 -0400

    Run nose for API server.

--- APIServer/pytests.sh
+++ APIServer/pytests.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+export user_type="test"
+export test_dir="tests"
+export ignores="FOO"  # dummy file!
 
-exit 0
+if [ -z $1 ]
+then
+    export capture=""
+else
+    export capture="--nocapture"
+fi
+
+nosetests --ignore-files=$ignores --exe --verbose --with-coverage --cover-package=models $capture
+
