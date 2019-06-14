4e79de5edc3b86486365cad8bbbb6fa4611aab9c
Denys Fenchenko <denisfench@gmail.com>
2019-06-14 13:41:04 -0400

    Adding agents to wolfram model

--- models/wolfram.py
+++ models/wolfram.py
@@ -1,8 +1,10 @@
 """
     This is rm -f ./.git/index.lock re-written in indra.
 """
-
+from propargs.propargs import PropArgs
+from indra.agent import Agent
 from indra.env import Env
+from indra.space import DEF_HEIGHT, DEF_WIDTH
 from indra.composite import Composite
 from indra.display_methods import BLACK, WHITE
 
@@ -11,6 +13,10 @@ Y = 1
 
 DEBUG = True  # turns debugging code on or off
 DEBUG2 = False  # turns deeper debugging code on or off
+
+# default number of agents 
+DEF_NUM_WHITE = 10
+DEF_NUM_BLACK = 10
 
 # states
 B = 1
@@ -41,9 +47,47 @@ def setup():
     return (black, white, wolframEnv)
 
 
+def agent_action(agent):
+    print("I'm " + agent.name + " and I'm acting.")
+    # return False means to move
+    return False
+
+
+def create_agent(color, i):
+    """
+    Create an agent.
+    """
+    return Agent(color + str(i), action=agent_action)
+
+
+def set_up():
+    """
+    A func to set up run that can also be used by test code.
+    """
+    pa = PropArgs.create_props('basic_props',
+                               ds_file='props/basic.props.json')
+    white_group = Composite("Whites", {"color": WHITE},
+                           member_creator=create_agent,
+                           num_members=pa.get('num_white', DEF_NUM_WHITE))
+    black_group = Composite("Blacks", {"color": BLACK},
+                          member_creator=create_agent,
+                          num_members=pa.get('num_black', DEF_NUM_BLACK))
+
+    env = Env("env",
+              height=pa.get('grid_height', DEF_HEIGHT),
+              width=pa.get('grid_width', DEF_WIDTH),
+              members=[white_group, black_group])
+    return (white_group, black_group, env)
+
+
 def main():
-    (black, white, wolframEnv) = setup()
-    wolframEnv()
+    global white_group
+    global black_group
+    global env
+    #(black, white, wolframEnv) = setup()
+    (black, white, env) = set_up()
+    #wolframEnv()
+    env()
     return 0
 
 
