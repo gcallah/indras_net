8d2efeded0c39f8916b7b30b79f5b03bf325c436
gcallah <gcallah@mac.com>
2019-06-17 18:11:25 -0400

    Building production.

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,10 @@
 # This shell file deploys a new version to our server.
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ~/.ssh/indra_rsa # Allow read access to the private key
-ssh-add ~/.ssh/indra_rsa # Add the private key to SSH
+chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
+ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 echo "SSHing to PythonAnywhere."
 ssh -o "StrictHostKeyChecking no" indrasnet@ssh.pythonanywhere.com << EOF
-    cd /home/indrasnet/indras_net
-    /home/indrasnet/indras_net/rebuild.sh
+    cd ~/indras_net; ~/indras_net/rebuild.sh
 EOF
