f92975b66695a88a4e24ffd9dd882a58132fcbd8
Denys Fenchenko <denisfench@gmail.com>
2019-06-18 16:03:54 -0400

    Changing space for wolfram

--- indra/space.py
+++ indra/space.py
@@ -258,7 +258,20 @@ class Space(Composite):
         """
         if row_num < 0 or row_num >= self.height:
             return None
-        # row = Composite("Row " + str(row_num))
+        else:
+            row = Composite("Row " + str(row_num))
+            for i in self.width:
+                neighbors_dict = {"neighbors": lst}
+                curr_agent = self.get_agent_at(i, row_num)
+                curr_agent_x = curr_agent.get_x()
+                for i in range (-1, 2):
+                    if not out_of_bounds(curr_agent_x, row_num +1, 0, 0,
+                                         self.width, self.height):neighbors_dict["neighbors"].append(self.get_agent_at(curr_agent_x, row_num+1))
+                curr_agent.neighbors = grp_from_nm_dict("Row neighbors", neighbors_dict["neighbors"])
+                row+= curr_agent 
+            return row
+                
+             
         # now loop through each col adding each agent from self.locations
         #  to the row, e.g., row += self.locations
 
