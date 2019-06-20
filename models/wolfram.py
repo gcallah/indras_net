7c3cc10a6586d6c6f2622ec0b5834cf2fbcfb222
Denys Fenchenko <denisfench@gmail.com>
2019-06-20 15:35:03 -0400

    Wolfram rules added

--- models/wolfram.py
+++ models/wolfram.py
@@ -38,6 +38,18 @@ GRID_HEIGHT = 30
 
 groups = []
 
+template = {}
+    
+rules = [
+    (B, B, B),
+    (B, B, W),
+    (B, W, B),
+    (B, W, W),
+    (W, B, B),
+    (W, B, W),
+    (W, W, B),
+    (W, W, W)
+]
 
 def create_agent(x, y):
     """
@@ -77,7 +89,31 @@ def get_color(group):
     else:
         return 1
 
+    
+def generate_wolfram_rules():
+    with open("wolfram_rules.txt","w+") as f: 
+        for i in range(256):
+            binary = bin(i + 256)[3:]
+            for j in range(len(binary)):
+                rule = str(rules[j])
+                template[rule] = int(binary[j])
+            f.write(str(template) + "\n")
 
+    print("256 rules are successfully generated")
+        
+def read_wolfram_rules(file_name):
+    rules_sets = []
+    with open(file_name, "r") as f:
+        all_rules = f.readlines()
+        for i in all_rules:
+            rules_sets.append(ast.literal_eval(i))
+
+    return rules_sets
+    
+generate_wolfram_rules()
+   #print(read_wolfram_rules("wolfram_rules.txt"))
+   
+    
 def check_rule(left, middle, right):
     """
     Takes in the current agent, the agent left and right to it,
