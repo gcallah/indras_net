c07560a4bfb4e1df6e1a6003fa4530d3b6241d83
phamleyennhi <npl243@nyu.edu>
2019-06-20 15:46:32 -0400

    get the value from the user

--- webapp/src/components/ModelDetail.js
+++ webapp/src/components/ModelDetail.js
@@ -5,14 +5,18 @@ import { Link } from 'react-router-dom';
 
 class ModelDetail extends Component {
   api_server = 'https://indrasnet.pythonanywhere.com/models/props/';
-  state = {
-    msg: '',
-    model_detail: {},
-    grid_height: {},
-    grid_width: {},
-    loadingData: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: '',
+      model_detail: {},
+      grid_height: {},
+      grid_width: {},
+      loadingData: false,
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   async componentDidMount() {
     this.setState({ loadingData: true });
     document.title = "Indra | Property";
@@ -22,13 +26,29 @@ class ModelDetail extends Component {
     this.setState({grid_height: res.data['grid_height']});
     this.setState({grid_width: res.data['grid_width']})
     this.setState({ loadingData: false });
-    console.log(this.state.grid_height['val']);
   }
-   handleChange = (event) => {
+  
+
+  handleChange = (event) => {
    //this.setState({ [target['val']]: target.value });
-   this.setState({grid_height: event.target.value});
-   console.log(this.state.grid_height['val']);
+   let newVar = JSON.parse(JSON.stringify(this.state[event.target.name]))
+   //make changes to ingredients
+    newVar['val'] = event.target.value 
+    this.setState({
+      [event.target.name] : newVar
+    }) 
+    console.log(this.state.grid_height);
+    console.log(this.state.grid_height['val']);
+
+    console.log(this.state.grid_width);
+    console.log(this.state.grid_width['val']);
   }
+
+  handleSubmit(event) {
+    alert('grid_height: ' + this.state.grid_height);
+    event.preventDefault();
+  }
+
   render() {
     if (this.state.loadingData) {
       return (
@@ -45,9 +65,12 @@ class ModelDetail extends Component {
         <h1 style={{ "textAlign": "left" }}> List of properties </h1>
         <br /><br />
         <form>
-		<label>Grid Height :</label>
-			<input type="int" defaultValue={this.state.grid_height['val']} onChange={this.updateState}/>
+		      <label> {this.state.grid_height['question']} :</label>
+			    <input type="int" defaultValue={this.state.grid_height['val']} name='grid_height' onChange={this.handleChange}/>
 	        <br /><br/>
+          <label> {this.state.grid_width['question']} :</label>
+          <input type="int" defaultValue={this.state.grid_height['val']} name='grid_width' onChange={this.handleChange}/>
+          <br /><br/>
         </form>
         <br /><br />
       </div>
