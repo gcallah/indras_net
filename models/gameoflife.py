1a0efc1800c57db8156c6e11ea1c634c6d17329e
Denys Fenchenko <denisfench@gmail.com>
2019-06-24 17:10:25 -0400

    Small modifications

--- models/gameoflife.py
+++ models/gameoflife.py
@@ -13,10 +13,6 @@ DEBUG = True  # Turns debugging code on or off
 
 groups = []
 
-pa = PropArgs.create_props('basic_props',
-                           ds_file='props/gameoflife.props.json')
-
-
 def create_agent(x, y):
     """
     Create an agent with the passed x, y value as its name.
@@ -90,6 +86,8 @@ def set_up():
     """
     A func to set up run that can also be used by test code.
     """
+    pa = PropArgs.create_props('basic_props',
+                               ds_file='props/gameoflife.props.json')    
     width = pa.get('grid_width', DEF_WIDTH)
     height = pa.get('grid_height', DEF_HEIGHT)
     black = Composite("black", {"color": BLACK, "marker": SQUARE})
