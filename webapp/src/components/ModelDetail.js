7d839a2dbf932fa23613d5f90c3e81b2408c688d
KhaingSuYin <ksy243@nyu.edu>
2019-06-24 10:59:35 -0700

    update this.state.model_detail in handleChange function

--- webapp/src/components/ModelDetail.js
+++ webapp/src/components/ModelDetail.js
@@ -1,7 +1,10 @@
+import 'rc-slider/assets/index.css';
 import React, { Component } from "react";
 import { Loader, Dimmer } from "semantic-ui-react";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Slider from 'rc-slider'
+
 
 class ModelDetail extends Component {
   api_server = 'https://indrasnet.pythonanywhere.com/models/props/';
@@ -9,7 +12,7 @@ class ModelDetail extends Component {
     super(props);
     this.state = {
       msg: '',
-      model_detail: {},
+      model_details: {},
       loadingData: false,
     }
     this.handleChange = this.handleChange.bind(this);
@@ -18,17 +21,17 @@ class ModelDetail extends Component {
   async componentDidMount() {
     this.setState({ loadingData: true });
     document.title = "Indra | Property";
-    const {menu_id} = this.props.location.state
-    const res = await axios.get(this.api_server + menu_id.id)
-    this.setState({ model_detail: res.data });
-    console.log(this.state.model_detail)
+    const {menu_id} = this.props.location.state;
+    const res = await axios.get(this.api_server + menu_id.id);
+    this.setState({ model_details: res.data });
     this.states(res.data);
+    console.log(this.state.grid_height['hival']);
     this.setState({ loadingData: false });
   }
   
   states(data){
     //loop over objects in data and create object in this.state
-    Object.keys(this.state.model_detail).forEach(item => 
+    Object.keys(this.state.model_details).forEach(item => 
          this.setState({[item]: data[item]})
                                        );
   }
@@ -37,16 +40,19 @@ class ModelDetail extends Component {
   Object.keys(this.state).forEach(item => console.log(1))}
 
 
-  handleChange = (event) => {
-   //this.setState({ [target['val']]: target.value });
-   let newVar = JSON.parse(JSON.stringify(this.state[event.target.name]))
-   //make changes to ingredients
-    newVar['val'] = event.target.value 
+handleChange = name => (e) => {
+    let newVar = JSON.parse(JSON.stringify(this.state[name]))
+    newVar['val'] = e
     this.setState({
-      [event.target.name] : newVar
-    }) 
-   console.log(this.state[event.target.name])
-  }
+      [name]: newVar 
+    });
+    let newVar1 = JSON.parse(JSON.stringify(this.state.model_details))
+    newVar1[name]= this.state[name]
+    this.setState({ model_details: newVar1 });
+    console.log(this.state.model_details)
+  };
+
+
 
   handleSubmit(event) {
     alert('grid_height: ' + this.state.grid_height);
@@ -69,11 +75,13 @@ class ModelDetail extends Component {
         <h1 style={{ "textAlign": "left" }}> List of properties </h1>
         <br /><br />
         <form>
-            {this.state.grid_height ?<label> {this.state.grid_height['question']} : <input type="int" defaultValue={this.state.grid_height['val']} onChange={this.handleChange} minValue ={this.state.grid_height['loval']} maxValue={this.state.grid_height['hival']} name='grid_height' /><br /><br /></label>: null}
-            {this.state.grid_width ? <label> {this.state.grid_width['question']} : <input type="int" defaultValue={this.state.grid_width['val']} name='grid_width' minValue ={this.state.grid_width['loval']} maxValue={this.state.grid_width['hival']} onChange={this.handleChange}/> <br /><br/></label> : null}
-
-         {this.state.num_blue ? <label> {this.state.num_blue['question']} : <input type="int"      defaultValue={this.state.num_blue['val']} onChange={this.handleChange} minValue ={this.state.num_blue['loval']} maxValue={this.state.num_blue['hival']} name='num_blue' /><br /><br /></label>: null}
-         {this.state.num_red ? <label> {this.state.num_red['question']} : <input type="int"      defaultValue={this.state.num_red['val']} minValue ={this.state.num_red['loval']} maxValue={this.state.num_red['hival']}  onChange={this.handleChange} name='num_red' /></label>: null}
+            {this.state.grid_height ?<div><label> {this.state.grid_height['question']} :   </label><Slider
+          min={this.state.grid_height['lowval']}
+          max={this.state.grid_height['hival']}
+          defaultValue={this.state.grid_height['val']}
+          onChange={this.handleChange('grid_height')}
+        /><br /><br /></div>: null}
+            
         </form>
         <br /><br />
       </div>
