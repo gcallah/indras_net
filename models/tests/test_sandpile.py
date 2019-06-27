24c913e51d7868b6096b4b6c49669ec37bc5d854
gcallah <gcallah@mac.com>
2019-06-27 14:07:39 -0400

    Building production.

--- models/tests/test_sandpile.py
+++ models/tests/test_sandpile.py
@@ -8,7 +8,8 @@ from models.sandpile import get_curr_group_idx, change_group, add_grain, topple,
 from models.sandpile import NUM_GROUPS, SAND_PREFIX
 import models.sandpile as sp
 
-TEST_ANUM = 999999
+TEST_X = 1
+TEST_Y = 1
 
 REP_RAND_TESTS = 20
 
@@ -18,36 +19,33 @@ sandpile = None
 
 class SandpileTestCase(TestCase):
     def setUp(self):
-        # (sp.groups, sp.group_indices, sp.sandpile) = set_up()
-        #self.test_agent = create_agent(TEST_ANUM, TEST_ANUM)
+        # self.test_agent = create_agent(TEST_X, TEST_Y)
         (sp.groups, sp.group_indices, sp.sandpile) = set_up()
-        pass
 
     def tearDown(self):
         self.test_agent = None
         sp.groups = None
         sp.group_indices = None
         sp.sandpile = None
-        pass
 
     def test_create_agent(self):
-        agent = create_agent(TEST_ANUM, TEST_ANUM)
-        test_name = "(" + str(TEST_ANUM) + "," + str(TEST_ANUM) + ")"
+        agent = create_agent(TEST_X, TEST_Y)
+        test_name = "(" + str(TEST_X) + "," + str(TEST_Y) + ")"
         self.assertEqual(agent.name, test_name)
 
     def test_get_curr_group_idx(self):
-        a = create_agent(TEST_ANUM, TEST_ANUM)
+        a = create_agent(TEST_X, TEST_Y)
         sp.groups = []
         sp.groups.append(Composite("Group" + str(0)))
         sp.groups[0]+= a
         self.assertEqual(get_curr_group_idx(a), 0)
 
     def test_get_next_group_idx(self):
-        a = create_agent(TEST_ANUM, TEST_ANUM)
+        a = create_agent(TEST_X, TEST_Y)
         self.assertEqual(get_next_group_idx(0), 1)
 
     def test_change_group(self):
-       a = create_agent(TEST_ANUM, TEST_ANUM)
+       a = create_agent(TEST_X, TEST_Y)
        sp.groups = []
        sp.groups.append(Composite("Group" + str(0)))
        sp.groups.append(Composite("Group" + str(1)))
@@ -56,7 +54,7 @@ class SandpileTestCase(TestCase):
        self.assertEqual(get_curr_group_idx(a), 1)
 
     def test_add_grain(self):
-        a = create_agent(TEST_ANUM, TEST_ANUM)
+        a = create_agent(TEST_X, TEST_Y)
         sp.groups = []
         sp.groups.append(Composite("Group" + str(0)))
         sp.groups.append(Composite("Group" + str(1)))
@@ -65,9 +63,9 @@ class SandpileTestCase(TestCase):
         self.assertEqual(a.primary_group(), sp.groups[1])
 
     def test_topple(self):
-        a = create_agent(TEST_ANUM, TEST_ANUM)
-        b = create_agent(TEST_ANUM - 1, TEST_ANUM)
-        c = create_agent(TEST_ANUM + 1, TEST_ANUM)
+        a = create_agent(TEST_X, TEST_Y)
+        b = create_agent(TEST_X - 1, TEST_Y)
+        c = create_agent(TEST_X + 1, TEST_Y)
         sp.groups = []
         sp.groups.append(Composite("Group" + str(0)))
         sp.groups.append(Composite("Group" + str(1)))
@@ -87,26 +85,27 @@ class SandpileTestCase(TestCase):
         sandpile_action(sp.sandpile)
         self.assertEqual(sp.sandpile.attrs["center_agent"].primary_group(), sp.groups[1])
 
-    # def test_place_action(self):
-    #     a = create_agent(TEST_ANUM, TEST_ANUM)
-    #     b = create_agent(TEST_ANUM - 1, TEST_ANUM)
-    #     c = create_agent(TEST_ANUM + 1, TEST_ANUM)
-    #     d = create_agent(TEST_ANUM, TEST_ANUM - 1)
-    #     e = create_agent(TEST_ANUM, TEST_ANUM + 1)
-    #     sp.groups = []
-    #     sp.groups.append(Composite("Group" + str(0)))
-    #     sp.groups[0] += a
-    #     sp.groups[0] += b
-    #     sp.groups[0] += c
-    #     sp.groups[0] += d
-    #     sp.groups[0] += e
-    #     sp.sandpile = Env("Sandpile", members=sp.groups)
-    #     lst = []
-    #     neighbors_dict = {"neighbors": lst}
-    #     neighbors_dict["neighbors"].append(b)
-    #     neighbors_dict["neighbors"].append(c)
-    #     neighbors_dict["neighbors"].append(d)
-    #     neighbors_dict["neighbors"].append(e)
-    #     neighbors_composite = grp_from_nm_dict("Vonneuman neighbors", neighbors_dict["neighbors"])
-    #     place_action(sp.sandpile, a)
-    #     self.assertEqual(a.neighbors, neighbors_composite)
+    def test_place_action(self):
+        a = create_agent(TEST_X, TEST_Y)
+        b = create_agent(TEST_X - 1, TEST_Y)
+        c = create_agent(TEST_X + 1, TEST_Y)
+        d = create_agent(TEST_X, TEST_Y - 1)
+        e = create_agent(TEST_X, TEST_Y + 1)
+        sp.groups = []
+        sp.groups.append(Composite("Group" + str(0)))
+        sp.groups[0] += a
+        sp.groups[0] += b
+        sp.groups[0] += c
+        sp.groups[0] += d
+        sp.groups[0] += e
+        sp.sandpile = Env("Sandpile", members=sp.groups)
+        correct_neighbors = Composite("Vonneuman neighbors")
+        correct_neighbors += b
+        correct_neighbors += c
+        correct_neighbors += d
+        correct_neighbors += e
+        place_action(sp.sandpile, a)
+        print("a.n = ", repr(a.neighbors))
+        print("cn = ", repr(correct_neighbors))
+        # I think you should loop like agent.py line 176
+        # self.assertEqual(a.neighbors, correct_neighbors)
