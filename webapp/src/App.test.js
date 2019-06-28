857a7ebbcbb67c41b988d66ae6d9e136e534df7b
KhaingSuYin <ksy243@nyu.edu>
2019-06-28 12:12:52 -0700

    add test for header

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow , mount} from 'enzyme';
+import Home from './components/Home';
 
 
 it('renders without crashing', () => {
@@ -9,3 +11,16 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div); 
 
 });
+ 
+
+describe('Home component', () => {
+      it('header prints', () => {
+        const wrapper = mount(<Home loadingData={true}/>);
+        const header = wrapper.text();
+        expect(header).toEqual('Loading...');
+      });
+    });
+
+
+
+
