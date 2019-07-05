174b0610b395527bbf21a3aedc78e3b984805d49
Simon <simonoh54@icloud.com>
2019-07-05 13:18:11 -0400

    Added comments to the tests

--- models/tests/test_gameoflife.py
+++ models/tests/test_gameoflife.py
@@ -22,21 +22,34 @@ class GameOfLifeTestCase(TestCase):
         pass
 
     def test_create_agent(self):
+        """
+        Creates an agent and checks that it has the correct name,
+        which is its (x, y) corrdinates.
+        """
         agent = create_agent(TEST_X, TEST_Y)
         test_name = "(" + str(TEST_X) + "," + str(TEST_Y) + ")"
         self.assertEqual(agent.name, test_name)
 
     def test_change_color(self):
-        # agent = create_agent(TEST_X, TEST_Y)
-        # groups = []
-        # groups.append(Composite("white"))
-        # groups.append(Composite("black"))
-        # g.groups[0] += agent
+        """
+        Changes a color of a white agent using change_color method
+        and checks that it is black.
+        Also changes it back to white and checks if it did.
+        """
         g.change_color(g.gameoflife_env.get_agent_at(0, 0))
         self.assertEqual((g.gameoflife_env.get_agent_at(0, 0)).primary_group(), g.groups[1])
         g.change_color(g.gameoflife_env.get_agent_at(0, 0))
+        self.assertEqual((g.gameoflife_env.get_agent_at(0, 0)).primary_group(), g.groups[0])
 
     def test_apply_live_rules(self):
+        """
+        Creates three agents.
+        All three agents are alive.
+        Assign one agent to be a neighbor to another agent, 
+        and check if apply_live_rules returns True.
+        Add another agent to be their neighbors,
+        and check if apply_live_rules returns False.
+        """
         a = create_agent(TEST_X, TEST_Y)
         b = create_agent(TEST_X - 1, TEST_Y)
         c = create_agent(TEST_X + 1, TEST_Y)
@@ -55,6 +68,14 @@ class GameOfLifeTestCase(TestCase):
         self.assertEqual(apply_live_rules(a), False)
 
     def test_apply_dead_rules(self):
+        """
+        Creates four agents.
+        Three agents are alive while the other is dead.
+        Assign two live agents to be neighbors to the dead agent, 
+        and check if apply_dead_rules returns False.
+        Add another agent to be their neighbors,
+        and check if apply_dead_rules returns True.
+        """
         a = create_agent(TEST_X, TEST_Y)
         b = create_agent(TEST_X - 1, TEST_Y)
         c = create_agent(TEST_X + 1, TEST_Y)
@@ -74,63 +95,3 @@ class GameOfLifeTestCase(TestCase):
         neighbors += d
         a.neighbors = neighbors
         self.assertEqual(apply_dead_rules(a), True)
-
-    def test_gameoflife_action(self):
-        a = create_agent(TEST_X, TEST_Y)
-        b = create_agent(TEST_X - 1, TEST_Y)
-        c = create_agent(TEST_X + 1, TEST_Y)
-        d = create_agent(TEST_X, TEST_Y + 1)
-        g.groups = []
-        g.groups.append(Composite("white"))
-        g.groups.append(Composite("black"))
-        g.groups[0] += a
-        g.groups[1] += b
-        g.groups[1] += c
-        g.groups[1] += d
-        neighbors_composite = Composite("neighbors_composite")
-        neighbors_composite += b
-        neighbors_composite += c
-        neighbors_composite += d
-        a.neighbors = neighbors_composite
-        gameoflife_action()
-        # self.assertEqual(a.primary_group(), g.groups[1])
-        # fix to check the colors
-        self.assertEqual(len(a.neighbors), 3)
-
-    # def test_agent_action(self):
-    #     a = create_agent(TEST_X, TEST_Y)
-    #     b = create_agent(TEST_X - 1, TEST_Y)
-    #     c = create_agent(TEST_X + 1, TEST_Y)
-    #     d = create_agent(TEST_X, TEST_Y + 1)
-    #     g.groups = []
-    #     g.groups.append(Composite("Group" + str(0)))
-    #     g.groups += a
-    #     g.groups += b
-    #     g.groups += c
-    #     g.groups += d
-    #     neighbors = Composite("neighbors")
-    #     neighbors += b
-    #     neighbors += c
-    #     neighbors += d
-    #     g.gameoflife_env = Env("gameoflife", members = g.groups)
-    #     agent_action(a)
-    #     for i in a.neighbors:
-    #     assertEqual(a.neighbors, neighbors)
-
-    # def test_populate_board_glider(self):
-    #     #self.assertEqual((g.gameoflife_env.get_agent_at(15, 15)).primary_group(), g.groups[0])
-    #     #self.assertEqual((g.gameoflife_env.get_agent_at(14, 16)).primary_group(), g.groups[0])
-    #     #self.assertEqual((g.gameoflife_env.get_agent_at(15, 14)).primary_group(), g.groups[0])
-    #     g.populate_board_glider(30, 30)
-    #     #self.assertEqual((g.gameoflife_env.get_agent_at(15, 15)).primary_group(), g.groups[1])
-    #     #self.assertEqual((g.gameoflife_env.get_agent_at(14, 16)).primary_group(), g.groups[1])
-    #     self.assertEqual((g.gameoflife_env.get_agent_at(15, 14)).primary_group(), g.groups[1])
-
-
-
-
-
-
-
-
-
