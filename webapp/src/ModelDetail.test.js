7376b25aa66bafd43b50c2f182611a71535b6687
phamleyennhi <npl243@nyu.edu>
2019-07-08 13:31:33 -0400

    render input test, handleSubmit function called test

--- webapp/src/ModelDetail.test.js
+++ webapp/src/ModelDetail.test.js
@@ -4,19 +4,47 @@ import ModelDetail from './components/ModelDetail';
 import { create } from "react-test-renderer";
 import axios from 'axios';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 
-describe("ModelDetail component", () => {
-  it("has Submit button",() => {
-    const component = mount(<ModelDetail {...props}/>);
-    expect(component.find('h2').text()).toEqual('Please set the parameters for your model')
+
+describe('ModelDetail Component', () => {
+
+  it('has an h2 tag', () => {
+    const component = shallow(<ModelDetail/>);    
+    var node = component.find('h2');
+    expect(node.length).toEqual(1);
   });
+
+    it('has an h3 tag', () => {
+    const component = shallow(<ModelDetail/>);    
+    var node = component.find('h3');
+    expect(node.length).toEqual(1);
+  });
+})
+
+it('calls handleSubmit function when form is submitted', () => {
+  const handleSubmit = jest.fn();
+  const wrapper = mount(<form onSubmit={handleSubmit}/>);
+  const form = wrapper.find('form');
+  form.simulate('submit');
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+});
+
+
+it('renders input', () => {
+  const wrapper = mount(<input type="item_type" defaultValue="default_value" name="item_name" />);
+  const input = wrapper.find('input');
+  expect(input).toHaveLength(1);
+  expect(input.prop('type')).toEqual('item_type');
+  expect(input.prop('defaultValue')).toEqual('default_value');
+  expect(input.prop('name')).toEqual('item_name');
 });
 
 
 describe("ModelDetail component", () => {
   it("change loadingData state after componentDidMount", async () => {
     const component = mount(<ModelDetail />);
-    const instance = component.instance();
+    const instance = component.getInstance();
     await instance.componentDidMount();
     expect(instance.state.loadingData).toBe(false)
   });
