f44eb78ed6ca05979ec87d4c1520f1f7d4142fbc
Simon <simonoh54@icloud.com>
2019-07-13 16:41:56 -0400

    Added differnt types of stores

--- models/bigbox.py
+++ models/bigbox.py
@@ -7,16 +7,16 @@ from indra.utils import get_prop_path
 from indra.agent import Agent
 from indra.env import Env
 from indra.composite import Composite
-from indra.display_methods import RED, BLUE
+from indra.display_methods import RED, GRAY
 
 MODEL_NAME = "Big Box"
 DEBUG = True
 
-DEF_WIDTH = 20
-DEF_HEIGHT = 20
+DEF_WIDTH = 30
+DEF_HEIGHT = 30
 
-CONSUMER = 100
-MP = 5
+CONSUMER = 150
+MP = 3
 
 town = None
 groups = None
@@ -30,7 +30,7 @@ def create_consumer(name, expense):
     return Agent(name=name, attrs=balance, action=consumer_action)
 
 
-def create_mp(name, fixed_expense, variable_expense, capital, inventory_size):
+def create_mp(name, store_type, expense):
     """
     Create a mom and pop store agent.
     Fixed expense is things like rent, electricity bills, etc.
@@ -39,10 +39,14 @@ def create_mp(name, fixed_expense, variable_expense, capital, inventory_size):
     Inventory size is the amounf of customers that the store can serve
     in a single period.
     """
-    store_books = {"fixed expense": fixed_expense,
-                   "variable expense": variable_expense,
-                   "capital": capital, "inventory": [inventory_size, 0]}
-    return Agent(name=name, attrs=store_books, action=mp_action)
+    store_books = {"fixed expense": expense[0],
+                   "variable expense": expense[1],
+                   "capital": expense[2],
+                   "inventory": [expense[3], expense[3]],
+                   "income": expense[4]}
+    return Agent(name=(store_type + ": " + name),
+                 attrs=store_books,
+                 action=mp_action)
 
 
 def town_action(town):
@@ -59,35 +63,41 @@ def town_action(town):
             curr_store = town.get_agent_at(x, y)
             if (curr_store is not None
                     and curr_store.primary_group() == groups[1]):
-                nearby_customers = town.get_moore_hood(curr_store)
+                nearby_customers = town.get_moore_hood(curr_store, radius=2)
                 if DEBUG:
-                    print(curr_store, "capital:", curr_store.attrs["capital"])
-                    print("     Num of customers:", len(nearby_customers))
+                    print(curr_store)
+                    print("     Location:", curr_store.get_pos())
+                    print("     Capital:", curr_store.attrs["capital"])
                     print("     Inventory size before:",
                           curr_store.attrs["inventory"][1])
+                num_nearby_customers = 0
                 for customer in nearby_customers:
                     if nearby_customers[customer].primary_group() == groups[0]:
-                        curr_store.attrs["capital"] = (
-                            (nearby_customers[customer]).attrs["expense"]
-                            + curr_store.attrs["capital"])
-                        curr_store.attrs["inventory"][1] += 1
+                        num_nearby_customers += 1
+                        curr_store.attrs["capital"] += (
+                            (nearby_customers[customer]).attrs["expense"])
+                        curr_store.attrs["inventory"][1] -= 1
                 curr_store.attrs["capital"] = (
                     curr_store.attrs["capital"]
                     - curr_store.attrs["fixed expense"])
                 if DEBUG:
+                    print("     Num of customers:", num_nearby_customers)
                     print("     Inventory size after:",
                           curr_store.attrs["inventory"][1])
+                    print("     Fixed expense:",
+                          curr_store.attrs["fixed expense"])
                     print("     Capital after:", curr_store.attrs["capital"])
-                if (curr_store.attrs["inventory"][1]
-                   >= curr_store.attrs["inventory"][0] - 8):
+                if (curr_store.attrs["inventory"][1] - num_nearby_customers
+                   <= 0):
                     curr_store.attrs["capital"] = (
                         curr_store.attrs["capital"]
                         - curr_store.attrs["variable expense"])
                     curr_store.attrs["inventory"][1] = (
                         curr_store.attrs["inventory"][1]
-                        - curr_store.attrs["inventory"][0])
+                        + curr_store.attrs["inventory"][0])
                 if curr_store.attrs["capital"] <= 0:
-                    curr_store.die()
+                    print(curr_store, "is out of buisness")
+                    # curr_store.die()
 
 
 def consumer_action(agent):
@@ -118,15 +128,23 @@ def set_up(props=None):
                                    prop_dict=props)
     width = pa.get('grid_width', DEF_WIDTH)
     height = pa.get('grid_height', DEF_HEIGHT)
-    consumer_group = Composite("Consumer", {"color": BLUE})
+    consumer_group = Composite("Consumer", {"color": GRAY})
     mp_group = Composite("Mom and pop", {"color": RED})
     groups = []
     groups.append(consumer_group)
     groups.append(mp_group)
     for i in range(0, CONSUMER):
-        groups[0] += create_consumer('Consumer ' + str(i), 50)
+        groups[0] += create_consumer("Consumer " + str(i), 50)
+    shops = {"Grocery store": [80, 30, 1000, 30, 40],
+             "Coffee shop": [30, 20, 700, 20, 15],
+             "Restaurant": [50, 25, 800, 25, 25]}
     for j in range(0, MP):
-        groups[1] += create_mp('Mom and pop ' + str(j), 80, 30, 1000, 30)
+        groups[1] += create_mp("Mom and pop " + str(j),
+                               "Grocery store", shops["Grocery store"])
+        groups[1] += create_mp("Mom and pop " + str(j),
+                               "Coffee shop", shops["Coffee shop"])
+        groups[1] += create_mp("Mom and pop " + str(j),
+                               "Restaurant", shops["Restaurant"])
     town = Env("Town",
                action=town_action,
                members=groups,
