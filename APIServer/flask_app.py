a4ba0a63e9f04653a43d602a58a97b217338ffad
Karen Zhou <zz2202@nyu.edu>
2019-07-22 15:01:22 -0400

    format

--- APIServer/flask_app.py
+++ APIServer/flask_app.py
@@ -39,7 +39,7 @@ def load_menu():
 
 def json_converter(object):
     return json.loads(json.dumps(object.to_json(),
-    cls=AgentEncoder, indent=4))
+                      cls=AgentEncoder, indent=4))
 
 
 @api.route('/hello')
