28224c36b62ce4f1009a818e6b836d67c93a329c
Denys Fenchenko <denisfench@gmail.com>
2019-07-29 14:03:16 -0400

    Changes to webapp

---
+++ webapp/src/components/ModelMenu.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ModelMenu extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                console.log("Testing ModelMenu Component")
+            </div>
+        );
+    }
+}
+ 
+export default ModelMenu;
