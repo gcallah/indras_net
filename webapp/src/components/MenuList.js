66798a3c0b35c0ed24c6dafd09404ab2134b7b43
gcallah <gcallah@mac.com>
2019-08-05 16:17:33 -0400

    Break up lines in text message area on react front end.

--- webapp/src/components/MenuList.js
+++ webapp/src/components/MenuList.js
@@ -135,9 +135,12 @@ class MenuList extends Component {
         </h1>
         <br /><br />
         <div class="card w-50 overflow-auto" style={{float: 'right', width: "18rem", height: "18rem"}}>
-        <h5 style={{textAlign: 'center', "fontSize": 16}} class="card-header bg-primary text-white">Update Status</h5>
+        <h5 style={{textAlign: 'center', "fontSize": 16}}
+            class="card-header bg-primary text-white">Update Status</h5>
         <div class="card-body">
+        <pre>
         {this.state.msg}
+        </pre>
         </div>
         </div>
           <ul class="list-group">
