cb5b84f2bdd71264c0aed0f7c44711d1138962fc
phamleyennhi <npl243@nyu.edu>
2019-08-06 15:42:17 -0400

    fixed PreFormTextBox

--- webapp/src/components/PreFormTextBox.js
+++ webapp/src/components/PreFormTextBox.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 
-const renderPreFormTextBox = (title, dispText) => {
-    <div>
-    <h5 style={{textAlign: 'center', "fontSize": 16}}
-        class="card-header bg-primary text-white">{title}</h5>
-    <div class="card-body">
-    <pre>
-    {dispText}
-    </pre>
-    </div>
-    </div>
+function renderPreFormTextBox(props){
+	return(
+      <div class="card w-50 overflow-auto" style={{float:'right', width:"18rem", height:"18rem"}}>
+      <h5 style={{textAlign: 'center', "fontSize": 16}}
+          class="card-header bg-primary text-white">{"Model Status"}</h5>
+      <div class="card-body">
+      <pre>
+      {props.msg}
+      </pre>
+      </div>
+      </div>
+	)
 }
 
+export default renderPreFormTextBox;
