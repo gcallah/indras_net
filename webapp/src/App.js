fb69e926d44664e95138f3430ef628ecaed3786f
gcallah <gcallah@mac.com>
2019-08-06 16:49:50 -0400

    Component for model status working.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,7 +5,6 @@ import Home from './components/Home';
 import WIP from './components/WIP';
 import ModelDetail from './components/ModelDetail'
 import MenuList from "./components/MenuList"
-import Action from "./components/Action"
 import NotFoundPage from './components/NotFoundPage';
 import ErrorCatching from './components/ErrorCatching';
 
@@ -19,7 +18,6 @@ class App extends Component {
             <Route exact path="/wip" component={WIP} />
             <Route exact path="/models/props/:id" component={ModelDetail} />
             <Route exact path="/models/menu/:id" component={MenuList} />
-            <Route exact path="/models/menu/" component={Action} />
             <Route exact path='/errorCatching' component={ErrorCatching} />
             <Route component={NotFoundPage} />
           </Switch>
