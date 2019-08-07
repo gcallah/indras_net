43359d2d5aa5df16dc62caaf944eba9fb4c642c2
Karen Zhou <zz2202@nyu.edu>
2019-08-07 17:00:02 -0400

    segregation is working now

--- APIServer/flask_app.py
+++ APIServer/flask_app.py
@@ -15,6 +15,7 @@ from models.fashion import fs_unrestorable
 from models.flocking import fl_unrestorable
 from models.fmarket import fm_unrestorable
 from models.scheduler import sd_unrestorable
+from models.segregation import sg_unrestorable
 
 
 ERROR = "Error:"
@@ -137,6 +138,8 @@ class Run(Resource):
             fm_unrestorable(v)
         elif v.name == "scheduler":
             sd_unrestorable(v)
+        elif v.name == "A city":
+            sg_unrestorable(v)
         v.runN(periods=run_time)
         return json_converter(v)
 
