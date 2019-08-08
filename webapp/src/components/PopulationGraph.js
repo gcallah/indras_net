696b5d35d1a7333b77cebdd5c97f1ecca0a5654f
phamleyennhi <npl243@nyu.edu>
2019-08-08 00:56:18 -0400

    fix conditions for fmarket model in front end

--- webapp/src/components/PopulationGraph.js
+++ webapp/src/components/PopulationGraph.js
@@ -2,28 +2,35 @@ import React from "react";
 import { LineChart } from 'react-chartkick'
 import 'chart.js'
 
+const FMARKET = 10
 function PopulationGraph(props) {
     if (props.loadingData){
-        let env=props.env_file['pop_hist']['pops']
-        let WIDTH=props.env_file['props']['grid_height']['val']
-        let HEIGHT=props.env_file['props']['grid_width']['val']
-        console.log(HEIGHT)
-        console.log(WIDTH)
-        console.log(env)
         var data=[]
-        Object.keys(env).map((group,i_group)=> {
-            console.log(props.env_file['members'][group]['attrs']['color'])
-            return(
-                data.push({name:group,color: props.env_file['members'][group]['attrs']['color'], data: {}}),
-                Object.keys(env[group]).map((member, i_member)=>{
-                    console.log(member)
-                    return(
-                        data[i_group]['data'][member] = env[group][i_member]
-                    )
-                })
-            )
-        });
-        
+        console.log("props.id = " + props.id)
+        if (props.id != FMARKET){
+            let env=props.env_file['pop_hist']['pops']
+            Object.keys(env).map((group,i_group)=> {
+                console.log(props.env_file['members'][group]['attrs']['color'])
+                return(
+                    data.push({name:group,color: props.env_file['members'][group]['attrs']['color'], data: {}}),
+                    Object.keys(env[group]).map((member, i_member)=>{
+                        console.log(member)
+                        return(
+                            data[i_group]['data'][member] = env[group][i_member]
+                        )
+                    })
+                )
+            });
+        }
+        else {
+            let period = props.env_file["pop_hist"]["periods"]
+            let data_hist = props.env_file["members"]["market_maker"]["attrs"]["price_hist"]
+            var i
+            data.push({name: "price history", data:{}})
+            for (i = 0; i < period; i++){
+                data[0]["data"][i] = data_hist[i]
+            }
+        }
         console.log(data)
         return (
             <div>
--- webapp/src/components/ScatterPlot.js
+++ webapp/src/components/ScatterPlot.js
@@ -2,8 +2,9 @@ import React from "react";
 import { ScatterChart } from 'react-chartkick'
 import 'chart.js'
 
+const FMARKET = 10
 function ScatterPlot(props) {
-    if (props.loadingData){
+    if (props.loadingData && props.id != FMARKET){
         let env=props.env_file['members']
         let WIDTH=props.env_file['props']['grid_height']['val']
         let HEIGHT=props.env_file['props']['grid_width']['val']
@@ -15,9 +16,11 @@ function ScatterPlot(props) {
             return(
                 data.push({name:env[group]['name'],color: env[group]['attrs']['color'], data: []}),
                 Object.keys(env[group]['members']).map((member, i_member)=>{
-                    return(
-                        data[i_group]['data'].push(env[group]['members'][member]['pos'])
-                    )
+                	if (env[group]['members'][member]['pos'] !== null){
+	                    return(
+	                        data[i_group]['data'].push(env[group]['members'][member]['pos'])
+	                    )
+                	}
                 })
             )
         });
@@ -29,9 +32,15 @@ function ScatterPlot(props) {
             </div>
         );
     }
+    else if (props.loadingData && props.id == FMARKET){
+        return(
+            <p> There is no scatter plot available for this model! </p>
+        )
+    }
     else{
         return(null)
-}};
+    }
+};
 
 
 export default ScatterPlot;
