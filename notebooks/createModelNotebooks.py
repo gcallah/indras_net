46253b22f405264394d285c3e493c7b226d73343
vishalprabhu <vishalprabhu@rocketmail.com>
2019-09-04 16:23:12 -0400

    CreateModelNotebooks now working until the set_up function

--- notebooks/createModelNotebooks.py
+++ notebooks/createModelNotebooks.py
@@ -10,42 +10,64 @@ import os
 
 nb = nbf.v4.new_notebook()
 filename = sys.argv[-1]
+nb['cells'] = []
 
 filepath = os.path.abspath('../models') + '/' + filename
 with open(filepath) as fp:
-    intro = """\
-    # My first automatic Jupyter Notebook
-    This is an auto-generated notebook."""
+    intro = """# This is the working of the """ + filename[:-3] + """ model."""
+
+    nb['cells'].append(nbf.v4.new_markdown_cell(intro))
 
     line = fp.readline()
-    importCode = ""
+
+    #  Skip comments
     if '"""' in line:
         line = fp.readline()
         while '"""' not in line:
             line = fp.readline()
         line = fp.readline()
 
+    nb['cells'].append(nbf.v4.new_markdown_cell(
+        "First we import all necessary files"))
+
     # Import block
+    importCode = ""
     line = fp.readline()
     while line.startswith("from ") or line.startswith("import "):
         importCode += line
         line = fp.readline()
 
-    nb['cells'] = [nbf.v4.new_markdown_cell(intro),
-                   nbf.v4.new_code_cell(importCode)]
+    importCode += "from models." + filename[:-3] + " import set_up"
+
+    nb['cells'].append(nbf.v4.new_code_cell(importCode))
+
+    nb['cells'].append(nbf.v4.new_markdown_cell("We then initialize global variables"))
+
+    # Globals block
+    while line == '\n':
+        line = fp.readline()
+
+    prev = line
+    globalCode = ""
+    while not line.startswith('def '):
+        globalCode += line
+        line = fp.readline()
+        if line == prev:
+            break
+        prev = line
+
+    nb['cells'].append(nbf.v4.new_code_cell(globalCode))
+
+    nb['cells'].append(nbf.v4.new_markdown_cell("Now we call the set_up function "
+                       "to set up the environment of the model"))
 
     # set_up block
-    while not line.startswith("def set_up("):
-        line = fp.readline()
-
     setupCode = ""
-    setupCode += line
-    line = fp.readline()
-    while "return " not in line:
-        setupCode += line
+    while "set_up()" not in line:
         line = fp.readline()
-    setupCode += line
 
+    code = line.split('    ')
+    setupCode += code[1]
     nb['cells'].append(nbf.v4.new_code_cell(setupCode))
 
     # Finish making notebook and add notebook to
