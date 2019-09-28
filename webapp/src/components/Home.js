fda125f0de35ab4e02927119184d86034f206e04
Denys Fenchenko <denisfench@gmail.com>
2019-09-28 13:11:32 -0400

    Making manu scalable for iphone screen

--- webapp/src/components/Home.js
+++ webapp/src/components/Home.js
@@ -110,7 +110,7 @@ class Home extends Component {
                             <div className="row">
                                 <div className="col">
                                     {Object.keys(this.state.allItems).map((item, i)=>
-                                    <p className="w-50 p-3 list-group-item list-group-item-action"
+                                    <li className="w-50 p-3 list-group-item list-group-item-action"
                                         key={i}>
                                         {console.log(this.state.allItems)}
                                         <Link to={{pathname: `/models/props/${this.state.allItems[item]['model ID']}`}}
@@ -122,7 +122,7 @@ class Home extends Component {
                                             this.state.allItems[item]['source'])}>
                                             {this.state.allItems[item]['name']}
                                         </Link>
-                                    </p>)}
+                                    </li>)}
                                 </div>
                             </div>
                         </ul>
