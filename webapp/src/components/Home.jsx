50b9267207de31e97fdba424ebaace9a39074dc6
Kenneth Lee <kennethslee.613@gmail.com>
2019-10-14 15:55:32 -0400

    eslint fixes

--- webapp/src/components/Home.jsx
+++ webapp/src/components/Home.jsx
@@ -1,79 +1,79 @@
-import React, { Component } from "react";
-import { Loader, Dimmer } from "semantic-ui-react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import Carousel from "./Carousel.js";
-import sandpile_img from "./images/Sandpile.jpg";
-import sandpile1_img from "./images/sandpile_2.png";
-import mandelobrot_img from "./images/mendelobrot_sq.jpg";
+import React, { Component } from 'react';
+import { Loader, Dimmer } from 'semantic-ui-react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Carousel from './Carousel';
+import sandpileImg from './images/Sandpile.jpg';
+import sandpile1Img from './images/sandpile_2.png';
+import mandelobrotImg from './images/mendelobrot_sq.jpg';
+
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      msg: "",
+      msg: '',
       allItems: [],
       loadingData: false,
       dataForCarousel: [
-        { image: sandpile_img, title: "by Seth Terashima" },
-        { image: sandpile1_img, title: "by Colt Browninga" },
-        { image: mandelobrot_img, title: "by Adam majewski" }
-      ]
+        { image: sandpileImg, title: 'by Seth Terashima' },
+        { image: sandpile1Img, title: 'by Colt Browninga' },
+        { image: mandelobrotImg, title: 'by Adam majewski' },
+      ],
     };
-    this.api_server = "https://indrasnet.pythonanywhere.com/";
+    this.api_server = 'https://indrasnet.pythonanywhere.com/';
   }
 
   async componentDidMount() {
     try {
       this.setState({ loadingData: true });
-      document.title = "Indra | Home";
-      const res = await axios.get(this.api_server + "models");
+      document.title = 'Indra | Home';
+      const res = await axios.get(`${this.api_server }models`);
       this.setState({ allItems: res.data });
       this.setState({ loadingData: false });
-      const doc = localStorage.getItem("doc");
-      this.setState({ doc: doc });
-      console.log("data-->", this.state.allItems);
+      const doc = localStorage.getItem('doc');
+      this.setState({ doc });
+      console.log('data-->', this.state.allItems);
     } catch (e) {
       console.log(e.message);
     }
   }
 
   openDescription = () => {
-    const link = "https://gcallah.github.io/indras_net/index.html";
+    const link = 'https://gcallah.github.io/indras_net/index.html';
     window.open(link);
   };
 
   renderShowDescription = () => {
-    console.log("renderShowDescription called");
+    console.log('renderShowDescription called');
     return (
-      <h1 style={{ fontSize: 16, fontWeight: "400" }}>
+      <h1 style={{ fontSize: 16, fontWeight: '400' }}>
         <a href="#" className="text-primary m-2" onClick={this.openDescription}>
-          View Project Description{" "}
-        </a>{" "}
+          View Project Description
+          {' '}
+        </a>
+        {' '}
       </h1>
     );
   };
 
   handleClick(id, name, source) {
     console.log(localStorage);
-    localStorage.setItem("menu_id", id);
-    localStorage.setItem("name", name);
-    localStorage.setItem("source", source);
+    localStorage.setItem('menu_id', id);
+    localStorage.setItem('name', name);
+    localStorage.setItem('source', source);
   }
 
-  renderHeader = () => {
-    return <h1 className="text-center">Indra Agent-Based Modeling System</h1>;
-  };
+  renderHeader = () => <h1 className="text-center">Indra Agent-Based Modeling System</h1>;
 
-  renderChooseModelProp = () => {
-    return (
-      <h1 style={{ fontSize: 16, fontWeight: "400" }}>
-        Please choose a model:{" "}
-      </h1>
-    );
-  };
+  renderChooseModelProp = () => (
+    <h1 style={{ fontSize: 16, fontWeight: '400' }}>
+      Please choose a model:
+      {' '}
+    </h1>
+  );
 
   render() {
-    const {loadingData, dataForCarousel, allItems} = this.state;
+    const { loadingData, dataForCarousel, allItems } = this.state;
     if (loadingData) {
       return (
         <Dimmer active inverted>
@@ -99,22 +99,20 @@ class Home extends Component {
                       <Link
                         to={{
                           pathname: `/models/props/${
-                            allItems[item]["model ID"]
-                          }`
+                            allItems[item]['model ID']
+                          }`,
                         }}
                         className="text-primary"
                         data-toggle="tooltip"
                         data-placement="top"
-                        title={allItems[item]["doc"]}
-                        onClick={() =>
-                          this.handleClick(
-                            allItems[item]["model ID"],
-                            allItems[item]["name"],
-                            allItems[item]["source"]
-                          )
-                        }
+                        title={allItems[item].doc}
+                        onClick={() => this.handleClick(
+                          allItems[item]['model ID'],
+                          allItems[item].name,
+                          allItems[item].source,
+                        )}
                       >
-                        {allItems[item]["name"]}
+                        {allItems[item].name}
                       </Link>
                     </li>
                   ))}
@@ -126,7 +124,7 @@ class Home extends Component {
           <div className="col-6">
             <Carousel
               speed={5000}
-              autoplay={true}
+              autoplay
               className="col-12"
               data={dataForCarousel}
             />
