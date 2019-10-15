365cd2344d3be520cfc3799baf159f3da91dc571
Kenneth Lee <kennethslee.613@gmail.com>
2019-10-14 20:59:10 -0400

    installed package react-bootstrap and added tooltips to home buttons

--- webapp/src/components/Home.jsx
+++ webapp/src/components/Home.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Loader, Dimmer } from 'semantic-ui-react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/ToolTip';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Carousel from './Carousel';
@@ -26,10 +29,9 @@ class Home extends Component {
     try {
       this.setState({ loadingData: true });
       document.title = 'Indra | Home';
-      const res = await axios.get(`${this.api_server }models`);
+      const res = await axios.get(`${this.api_server}models`);
       this.setState({ allItems: res.data });
       this.setState({ loadingData: false });
-      console.log('data-->', this.state.allItems);
     } catch (e) {
       console.log(e.message);
     }
@@ -46,7 +48,6 @@ class Home extends Component {
   );
 
   handleClick(id, name, source) {
-    console.log(localStorage);
     localStorage.setItem('menu_id', id);
     localStorage.setItem('name', name);
     localStorage.setItem('source', source);
@@ -76,38 +77,36 @@ class Home extends Component {
         <div className="row">
           <div className="col-6">
             {this.renderChooseModelProp()}
-            <ul className="list-group">
-              <div className="row">
-                <div className="col">
-                  {Object.keys(allItems).map((item, i) => (
-                    <li
-                      className="w-75 p-3 list-group-item list-group-item-action"
-                      key={i}
-                    >
-                      {console.log(allItems)}
-                      <Link
-                        to={{
-                          pathname: `/models/props/${
-                            allItems[item]['model ID']
-                          }`,
-                        }}
-                        className="text-primary"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title={allItems[item].doc}
-                        onClick={() => this.handleClick(
-                          allItems[item]['model ID'],
-                          allItems[item].name,
-                          allItems[item].source,
-                        )}
-                      >
-                        {allItems[item].name}
-                      </Link>
-                    </li>
-                  ))}
-                </div>
-              </div>
-            </ul>
+            <ListGroup>
+              {Object.keys(allItems).map((item) => (
+                <OverlayTrigger
+                  key={`${allItems[item].name}-tooltip`}
+                  placement="right"
+                  overlay={(
+                    <Tooltip>
+                      {allItems[item].doc}
+                    </Tooltip>
+                  )}
+                >
+                  <Link
+                    to={{
+                      pathname: `/models/props/${
+                        allItems[item]['model ID']
+                      }`,
+                    }}
+                    className="text-primary w-75 p-3 list-group-item list-group-item-action"
+                    key={allItems[item].name}
+                    onClick={() => this.handleClick(
+                      allItems[item]['model ID'],
+                      allItems[item].name,
+                      allItems[item].source,
+                    )}
+                  >
+                    {allItems[item].name}
+                  </Link>
+                </OverlayTrigger>
+              ))}
+            </ListGroup>
             {this.renderShowDescription()}
           </div>
           <div className="col-6">
