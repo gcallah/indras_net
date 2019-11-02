7a089a5207034202c620a2b3fd87d9f0d76b0937
David Tsai <david@wecsam.com>
2019-11-02 13:34:03 -0500

    Revert "Updated api.sh to reflect..." because of failed build
    This reverts commit e013611c28f8e9bbd2261620b52c63c04b8e700d.

--- APIServer/api.sh
+++ APIServer/api.sh
@@ -2,4 +2,4 @@
 # The user_type env var is needed to make user interactions within
 # Indra behave properly.
 export user_type="api"
-FLASK_APP=api_endpoints flask run --host=127.0.0.1 --port=8000
+python3 api_endpoints.py
