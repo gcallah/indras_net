10790d4b0ed15402446e5188d2f22713c808dae4
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-03 21:24:10 -0500

    fixed eslint issues in Carousel.js

--- webapp/src/components/Carousel.js
+++ webapp/src/components/Carousel.js
@@ -1,60 +1,66 @@
-import React, { Component } from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 import propTypes from 'prop-types';
 
 class Carousel extends Component {
+  renderImage = () => {
+    const {
+      dots, speed, autoplay, className, data,
+    } = this.props;
+    const settings = {
+      dots,
+      infinite: true,
+      speed,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay,
+      fade: true,
+      className,
+    };
+    return (
+      <div>
+        <Slider {...settings}/* eslint-disable-line react/jsx-props-no-spreading */>
+          {data.map((item) => (
+            <div key={item.title}>
+              <img
+                src={item.image}
+                className="rounded-circle carousel"
+                alt="Responsive Model"
+                data-toggle="tooltip"
+                data-placement="top"
+                title={item.title}
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
+  }
 
-    constructor(props) {
-        super(props)
-    }
-
-    renderImage = () => {
-        let settings = {
-              dots: this.props.dots,
-              infinite: true,
-              speed: this.props.speed,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              autoplay: this.props.autoplay,
-              fade:true,
-              className: this.props.className,
-            };
-        return <div>
-            <Slider {...settings}>
-                 {this.props.data.map((item,index) => {
-                     return <div key ={index}>
-                        <img src={item['image']}
-                        className="rounded-circle carousel"
-                        alt="Responsive image"
-                        data-toggle="tooltip" data-placement="top" title={item['title']}/>
-                    </div>
-                })}
-            </Slider>
-        </div>
-    }
-
-    render() {
-        return (
-             <div >
-                 {this.renderImage()}
-             </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.renderImage()}
+      </div>
+    );
+  }
 }
 
-Carousel.propTypes ={
-    dots: propTypes.bool,
-    speed: propTypes.number,
-    autoplay: propTypes.bool,
-    data: propTypes.array,
-}
+Carousel.propTypes = {
+  dots: propTypes.bool,
+  speed: propTypes.number,
+  autoplay: propTypes.bool,
+  data: propTypes.arrayOf,
+};
 
-Carousel.defaultProps={
-    dots: false,
-    speed:1000,
-    autoplay: false
-}
+Carousel.defaultProps = {
+  dots: false,
+  speed: 1000,
+  autoplay: false,
+  data: [],
+};
 
 export default Carousel;
