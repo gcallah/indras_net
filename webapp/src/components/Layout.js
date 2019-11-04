14598aa3780a0a0ed9e1b13ecdc3590ea08b6419
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-03 21:50:53 -0500

    eslint fix of layout.js

--- webapp/src/components/Layout.js
+++ webapp/src/components/Layout.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import PropType from 'prop-types';
 import Header from './Header';
 
-export default props => {
-	return (
-		<Container>
-		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"></link>
-		<Header />
-		{props.children}
-		</Container>
-	);
+function Layout(props) {
+  const { children } = props;
+  return (
+    <Container>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
+      <Header />
+      {children}
+    </Container>
+  );
+}
+
+Layout.propTypes = {
+  children: PropType.shape(),
 };
+
+Layout.defaultProps = {
+  children: {},
+};
+
+export default Layout;
