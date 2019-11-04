39be3b9284b6ae436afcaff957025a856bd37e27
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-03 21:55:09 -0500

    header.js named the functional component

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
-export default () => (
-  <div>
-    <Menu.Item><Link to="/" /></Menu.Item>
-    <Menu.Menu position="right">
-      <Menu.Item>
-        <Link to="/" />
-      </Menu.Item>
-    </Menu.Menu>
-  </div>
-);
+function Header() {
+  return (
+    <div>
+      <Menu.Item><Link to="/" /></Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Link to="/" />
+        </Menu.Item>
+      </Menu.Menu>
+    </div>
+  );
+}
+
+export default Header;
