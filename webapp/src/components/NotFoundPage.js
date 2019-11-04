e7d588f2c1394210703452310d12eb26dcff8bb9
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-04 11:29:00 -0500

    autofix app, apptest, modeldetail, modelinput, modelstatus, notfoudnpage, pageloader, and preformtext

--- webapp/src/components/NotFoundPage.js
+++ webapp/src/components/NotFoundPage.js
@@ -4,25 +4,25 @@ import { Route } from 'react-router';
 import './NotFoundPage.css';
 
 const Status = ({ code, children }) => (
-    <Route
-        render={({ staticContext }) => {
-        if (staticContext) staticContext.status = code;
-            return children;
-        }}
-    />
+  <Route
+    render={({ staticContext }) => {
+      if (staticContext) staticContext.status = code;
+      return children;
+    }}
+  />
 );
 
 const NotFoundPage = () => (
-    <Status code={404}>
-        <div className="NotFoundPage">
-            <h1>Oops!</h1>
-            <div>Looks like you're lost...</div>
-            <div className="action">
-                <a className="btn btn-primary" href="/">
+  <Status code={404}>
+    <div className="NotFoundPage">
+      <h1>Oops!</h1>
+      <div>Looks like you're lost...</div>
+      <div className="action">
+        <a className="btn btn-primary" href="/">
                 Guide me to the right path!
-                </a>
-            </div>
-        </div>
-    </Status>
+        </a>
+      </div>
+    </div>
+  </Status>
 );
 export default NotFoundPage;
--- webapp/src/components/PreFormTextBox.js
+++ webapp/src/components/PreFormTextBox.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 function PreFormTextBox(title, text) {
-    return (
+  return (
     <div>
-        <h5 style={{textAlign: 'center', "fontSize": 16}}
-            className="card-header bg-primary text-white">{ title }</h5>
-        <div className="card-body">
-        <p class="card-text">
-        {text}
+      <h5
+        style={{ textAlign: 'center', fontSize: 16 }}
+        className="card-header bg-primary text-white"
+      >
+        { title }
+      </h5>
+      <div className="card-body">
+        <p className="card-text">
+          {text}
         </p>
-        </div>
-    </div>);
+      </div>
+    </div>
+  );
 }
 
 export default PreFormTextBox;
