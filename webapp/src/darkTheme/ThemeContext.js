d44ef54da8285e76460db8835ef43e15027dc391
Sabrena Beck <sb5829@nyu.edu>
2019-11-13 01:21:45 -0500

    added a night mode switch option at the left top corner

---
+++ webapp/src/darkTheme/ThemeContext.js
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { backgroundColor, textColor } from './theme';
+
+const ThemeToggleContext = React.createContext();
+
+export const useTheme = () => React.useContext(ThemeToggleContext);
+
+export const MyThemeProvider = ({ children }) => {
+  const [themeState, setThemeState] = React.useState({
+    mode: 'light',
+  });
+
+  const Wrapper = styled.div`
+    background-color: ${backgroundColor};
+    color: ${textColor};
+  `;
+
+  const toggle = () => {
+    const mode = (themeState.mode === 'light' ? 'dark' : 'light');
+    setThemeState({ mode });
+  };
+
+  return (
+    <ThemeToggleContext.Provider value={{ toggle }}>
+      <ThemeProvider
+        theme={{
+          mode: themeState.mode,
+        }}
+      >
+        <Wrapper>
+          {children}
+        </Wrapper>
+      </ThemeProvider>
+    </ThemeToggleContext.Provider>
+  );
+};
+
+export default ThemeProvider;
