2e052ae49a061c3676a238a92de73847620dcae2
Sabrena Beck <sb5829@nyu.edu>
2019-11-13 01:20:05 -0500

    Added a night mode option switch at the left top corner

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,6 +1,9 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+/* eslint-disable arrow-parens */
+import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import styled, { withTheme } from 'styled-components';
+import { Switch as Switch2 } from '@material-ui/core';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import WIP from './components/WIP';
@@ -8,10 +11,31 @@ import ModelDetail from './components/ModelDetail';
 import ActionMenu from './components/ActionMenu';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorCatching from './components/ErrorCatching';
+import { useTheme } from './darkTheme/ThemeContext';
 
-class App extends Component {
-  render() {
-    return (
+const Wrapper = styled('div')`
+  background: ${props => props.theme.background};
+  width: 100vw;
+  height: 100vh;
+  font-family: -apple-stem, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen";
+  h1 {
+    color: ${props => props.theme.body};
+  }
+`;
+
+function App() {
+  const themeState = useTheme();
+  return (
+    <Wrapper>
+      <div>
+        <Switch2
+          defaultChecked
+          value="checkedF"
+          color="default"
+          inputProps={{ 'aria-label': 'checkbox with default color' }}
+          onClick={() => themeState.toggle()}
+        />
+      </div>
       <HashRouter>
         <Layout>
           <Switch>
@@ -24,8 +48,8 @@ class App extends Component {
           </Switch>
         </Layout>
       </HashRouter>
-    );
-  }
+    </Wrapper>
+  );
 }
 
-export default App;
+export default withTheme(App);
