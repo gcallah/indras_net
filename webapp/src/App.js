c37e58350cbccc01bafd7eaa8689f271fa7b7265
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-16 11:36:29 -0500

    changed App.js to functional component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
@@ -9,23 +8,21 @@ import ActionMenu from './components/ActionMenu';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorCatching from './components/ErrorCatching';
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/wip" component={WIP} />
-            <Route exact path="/models/props/:id" component={ModelDetail} />
-            <Route exact path="/models/menu/:id" component={ActionMenu} />
-            <Route exact path="/errorCatching" component={ErrorCatching} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </Layout>
-      </HashRouter>
-    );
-  }
-}
+function App() {
+  return (
+    <HashRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/wip" component={WIP} />
+          <Route exact path="/models/props/:id" component={ModelDetail} />
+          <Route exact path="/models/menu/:id" component={ActionMenu} />
+          <Route exact path="/errorCatching" component={ErrorCatching} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Layout>
+    </HashRouter>
+  );
+};
 
 export default App;
