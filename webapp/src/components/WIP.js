aee4fc43ae926da895d32107c8576df695ce9188
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-16 11:35:20 -0500

    fixed WIP.js eslint errors

--- webapp/src/components/WIP.js
+++ webapp/src/components/WIP.js
@@ -1,46 +1,46 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/no-unused-state */
-/* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
 import { Loader, Dimmer } from 'semantic-ui-react';
 
 class WIP extends Component {
-    state = {
-      msg: '',
+  constructor(props) {
+    super(props);
+    this.state = {
       loadingData: false,
-    }
+    };
+  }
 
-    async componentDidMount() {
-      this.setState({ loadingData: true });
-      document.title = 'Indra | Work in Progress';
-      this.setState({ loadingData: false });
-    }
+  async componentDidMount() {
+    this.setState({ loadingData: true });
+    document.title = 'Indra | Work in Progress';
+    this.setState({ loadingData: false });
+  }
 
-    render() {
-      if (this.state.loadingData) {
-        return (
-          <Dimmer active inverted>
-            <Loader size="massive">Loading...</Loader>
-          </Dimmer>
-        );
-      }
-
+  render() {
+    const { loadingData } = this.state;
+    if (loadingData) {
       return (
-        <div>
-          <br />
-          <h1 style={{ textAlign: 'center' }}>
-Welcome to the Indra ABM platform!
-          </h1>
-          <br />
-          <br />
-
-          <p>We will have this model running soon!</p>
-
-          <br />
-          <br />
-        </div>
+        <Dimmer active inverted>
+          <Loader size="massive">Loading...</Loader>
+        </Dimmer>
       );
     }
+
+    return (
+      <div>
+        <br />
+        <h1 style={{ textAlign: 'center' }}>
+          Welcome to the Indra ABM platform!
+        </h1>
+        <br />
+        <br />
+
+        <p>We will have this model running soon!</p>
+
+        <br />
+        <br />
+      </div>
+    );
+  }
 }
 
 export default WIP;
