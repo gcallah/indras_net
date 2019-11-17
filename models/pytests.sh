1ccc757b132d1fa2c7c6670a66ff24b1a92a00b1
gcallah <gcallah@mac.com>
2019-11-17 11:54:27 -0500

    Experimenting with build to find cause of failure.

--- models/pytests.sh
+++ models/pytests.sh
@@ -9,6 +9,6 @@ else
     export capture="--nocapture"
 fi
 
-# nosetests --ignore-files=$ignores --exe --verbose $capture
-nosetests --ignore-files=$ignores --exe --verbose --with-coverage --cover-package=models $capture
+nosetests --ignore-files=$ignores --exe --verbose $capture
+# nosetests --ignore-files=$ignores --exe --verbose --with-coverage --cover-package=models $capture
 
