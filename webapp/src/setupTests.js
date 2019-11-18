342a1677a1ff48081b55cddb06dc6409d3ae83fc
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-18 12:19:48 -0500

    added to setupTests.js  to fix slider import problem in App.test.js

--- webapp/src/setupTests.js
+++ webapp/src/setupTests.js
@@ -2,3 +2,9 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+window.matchMedia = window.matchMedia || (() => ({
+  matches: false,
+  addListener() {},
+  removeListener() {},
+}));
