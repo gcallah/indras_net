06603580c0d66319979ac0747b2e4c465adf730b
gcallah <gcallah@mac.com>
2019-11-19 02:34:52 -0500

    Added comment as to why coverage is skipped in models tests.

--- models/pytests.sh
+++ models/pytests.sh
@@ -9,6 +9,7 @@ else
     export capture="--nocapture"
 fi
 
+# nose isn't finding tests at present, so we need to skip coverage:
 nosetests --ignore-files=$ignores --exe --verbose $capture
-#?nosetests --ignore-files=$ignores --exe --verbose --with-coverage --cover-package=models $capture
+# nosetests --ignore-files=$ignores --exe --verbose --with-coverage --cover-package=models $capture
 
