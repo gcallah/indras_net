e5920b965a79ea63c990282dcbfdf21bf9143bae
Philip Jung <philip.jung.98@gmail.com>
2019-11-19 01:00:45 -0500

    added a test case to ActionMenu.test file

--- webapp/src/ActionMenu.test.js
+++ webapp/src/ActionMenu.test.js
@@ -3,13 +3,18 @@ import { mount } from 'enzyme';
 import ActionMenu from './components/ActionMenu';
 
 describe('Mounted ActionMenu', () => {
+  const wrapper = mount(<ActionMenu />);
   it('should render Population Graph on Click', () => {
-    // Render a checkbox with label in the document
-    const wrapper = mount(<ActionMenu />);
     // const button = wrapper.find('btn-success').first();
     // button.simulate('click');
     console.log(wrapper.debug());
     const state = wrapper.state('loadingSourceCode');
     expect(state).toEqual(false);
+  });
+  it('should return home on home button click', () => {
+    global.window = { location: { pathname: null } };
+    const button = wrapper.find('BackHomeButton').first();
+    button.simulate('click');
+    expect(global.window.location.pathname).toEqual('/');
   });
 });
