3ba0924dd95bf4936be5db8e358132d0a3f8ae35
Selina Zhang <yz4184@nyu.edu>
2019-11-20 02:39:39 -0500

    corrected set_emoji_indicator and added more testcases

--- ml/tests/test_used_cars.py
+++ ml/tests/test_used_cars.py
@@ -42,8 +42,43 @@ class UsedCarTestCase(TestCase):
         self.assertGreaterEqual(result, MIN_CAR_LIFE)
         self.assertLessEqual(result, MAX_BAD_CAR_LIFE)
         
-    
-
+    def test_get_dealer_characteristic(self):
+        charac = get_dealer_characteristic()
+        self.assertTrue(charac == "good" or charac == "bad")
+        
+    def test_set_emoji_indicator(self):
+        buyer1 = create_buyer("Buyers",0, None)
+        buyer2 = create_buyer("Buyers",1, None)
+        buyer1["emoji_life_avg"] = {"unnatural":1}
+        buyer2["emoji_life_avg"] = {"relaxing":5}
+        set_emoji_indicator(buyer1)
+        set_emoji_indicator(buyer2)
+        self.assertEqual(buyer1["emoji_indicator"]["unnatural"], "bad")
+        self.assertEqual(buyer2["emoji_indicator"]["relaxing"], "good")
+        
+    def test_get_dealer_emoji(self):
+        emoji1 = get_dealer_emoji("good")
+        emoji2 = get_dealer_emoji("bad")
+        self.assertIn(emoji1, POS_EMOJIS)
+        self.assertIn(emoji2, NEG_EMOJIS)
+        
+    def test_update_dealer_sale(self):
+        dealer1 = create_dealer("Dealers", 0, None)
+        dealer2 = create_dealer("Dealers", 1, None)
+        num_sales = random.randint(1,100)
+        dealer2["num_sales"] = num_sales
+        new_car_life = random.randint(0,5)
+        avg_life = round(random.randrange(1,5),2)
+        dealer2["avg_car_life_sold"] = avg_life
+        update_dealer_sale(dealer1, new_car_life)
+        update_dealer_sale(dealer2, new_car_life)
+        self.assertEqual(dealer1["num_sales"], 1 )
+        self.assertEqual(dealer2["num_sales"], num_sales + 1)
+        self.assertEqual(dealer1["num_sales"], 1 )
+        self.assertEqual(dealer1["avg_car_life_sold"],new_car_life)
+        self.assertEqual(dealer2["avg_car_life_sold"],
+                         round((avg_life + new_car_life) / (num_sales + 1), 2))
+        
     def test_is_mature(self):
         buyer = create_buyer("Buyer", 0, None)
         self.assertFalse(is_mature(buyer))
@@ -54,18 +89,18 @@ class UsedCarTestCase(TestCase):
     def test_create_dealer(self):
         dealer = create_dealer("Dealer", 0, None)
         self.assertEqual(dealer["num_sales"], 0)
-        self.assertEqual(dealer["avg_car_life_sold"], None)
+        self.assertIsNone(dealer["avg_car_life_sold"])
         self.assertEqual(dealer["curr_car_life"], 0)
         self.assertEqual(dealer["return_rate"], 0)
         self.assertEqual(dealer["num_completed_services"], 0)  
-        self.assertEqual(dealer["emoji_used"], None)
-        self.assertEqual(dealer["dealer_characteristic"], None)
+        self.assertIsNone(dealer["emoji_used"])
+        self.assertIsNone(dealer["dealer_characteristic"])
     
     def test_create_buyer(self):
         buyer = create_buyer("Buyer", 0, None)
         self.assertFalse(buyer["has_car"])
-        self.assertEqual(buyer["car_life"], None)
-        self.assertEqual(buyer["interaction_res"], None)
+        self.assertIsNone(buyer["car_life"])
+        self.assertIsNone(buyer["interaction_res"])
         self.assertEqual(buyer["dealer_hist"], [])
         self.assertEqual(buyer["emoji_carlife_assoc"], {})
         self.assertEqual(buyer["emoji_life_avg"], {})  
