86294c3000b30d84c109bc64573efc8d3c7067fd
Kenneth Lee <kennethslee.613@gmail.com>
2019-11-24 11:59:14 -0500

    Updates

---
+++ precache-manifest.d134d7ac37e393e973b70deda4384d77.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29957200b32a549fe18424e20a15f9e",
+    "url": "/indras_net/index.html"
+  },
+  {
+    "revision": "f9aed39741fd108ba971",
+    "url": "/indras_net/static/css/2.057e60d6.chunk.css"
+  },
+  {
+    "revision": "740d30c4a190500a1633",
+    "url": "/indras_net/static/css/main.fd643c77.chunk.css"
+  },
+  {
+    "revision": "f9aed39741fd108ba971",
+    "url": "/indras_net/static/js/2.8e4aee22.chunk.js"
+  },
+  {
+    "revision": "740d30c4a190500a1633",
+    "url": "/indras_net/static/js/main.887c6d5c.chunk.js"
+  },
+  {
+    "revision": "a51c6d74b5c00dece6de",
+    "url": "/indras_net/static/js/runtime-main.f17cb719.js"
+  },
+  {
+    "revision": "090bc1a004cc85c7a9432489536ea04d",
+    "url": "/indras_net/static/media/Sandpile.090bc1a0.jpg"
+  },
+  {
+    "revision": "875dd8b84ac85ca1bc528cc62f140231",
+    "url": "/indras_net/static/media/mendelobrot_sq.875dd8b8.jpg"
+  },
+  {
+    "revision": "d38b3fb15132329ad50c0ece9add51e7",
+    "url": "/indras_net/static/media/sandpile_2.d38b3fb1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/indras_net/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/indras_net/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/indras_net/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/indras_net/static/media/slick.f97e3bbf.svg"
+  }
+]);
