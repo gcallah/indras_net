7238515d45a738431dbe26de09cc985d562f772e
Denys Fenchenko <denisfench@gmail.com>
2019-11-24 18:13:38 -0500

    Fixing build errors

--- webapp/src/components/Home.js
+++ webapp/src/components/Home.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Loader, Dimmer} from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Loader, Dimmer } from 'semantic-ui-react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
@@ -13,16 +13,16 @@ import mandelobrotImg from './images/mendelobrot_sq.jpg';
 import './styles.css';
 
 class Home extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
       allItems: [],
       loadingData: false,
       apiFailed: false,
       dataForCarousel: [
-        {image: sandpileImg, title: 'by Seth Terashima'},
-        {image: sandpile1Img, title: 'by Colt Browninga'},
-        {image: mandelobrotImg, title: 'by Adam majewski'},
+        { image: sandpileImg, title: 'by Seth Terashima' },
+        { image: sandpile1Img, title: 'by Colt Browninga' },
+        { image: mandelobrotImg, title: 'by Adam majewski' },
       ],
     };
     this.api_server = 'https://indrasnet.pythonanywhere.com/';
@@ -31,20 +31,20 @@ class Home extends Component {
   async componentDidMount() {
     const { history } = this.props;
     try {
-      this.setState ({loadingData: true});
+      this.setState({ loadingData: true });
       document.title = 'Indra | Home';
-      const res = await axios.get (`${this.api_server}models`);
-      this.setState ({allItems: res.data, loadingData: false});
+      const res = await axios.get(`${this.api_server}models`);
+      this.setState({ allItems: res.data, loadingData: false });
     } catch (e) {
       history.push('/errorCatching');
     }
   }
 
   handleClick = (id, name, source) => {
-    localStorage.setItem ('menu_id', id);
-    localStorage.setItem ('name', name);
-    localStorage.setItem ('source', source);
-  };
+    localStorage.setItem('menu_id', id);
+    localStorage.setItem('name', name);
+    localStorage.setItem('source', source);
+  }
 
   renderChooseModelProp = () => (
     <h1 className="small-header">
@@ -53,10 +53,14 @@ class Home extends Component {
     </h1>
   );
 
-  render () {
-    const {loadingData, dataForCarousel, allItems, apiFailed} = this.state;
+  render() {
+    const {
+      loadingData, dataForCarousel, allItems, apiFailed,
+    } = this.state;
     if (apiFailed) {
-      return <h1>404 Error</h1>;
+      return (
+        <h1>404 Error</h1>
+      );
     }
     if (loadingData) {
       return (
@@ -72,30 +76,31 @@ class Home extends Component {
         </div>
         <div className="row">
           <div className="col-6">
-            {this.renderChooseModelProp ()}
+            {this.renderChooseModelProp()}
             <ListGroup>
-              {Object.keys (allItems).map (item => (
+              {Object.keys(allItems).map((item) => (
                 <OverlayTrigger
                   key={`${allItems[item].name}-tooltip`}
                   placement="right"
-                  overlay={
+                  overlay={(
                     <Tooltip>
                       {allItems[item].doc}
                     </Tooltip>
-                  }
+                  )}
                 >
                   <Link
                     to={{
-                      pathname: `/models/props/${allItems[item]['model ID']}`,
+                      pathname: `/models/props/${
+                        allItems[item]['model ID']
+                      }`,
                     }}
                     className="text-primary w-75 p-3 list-group-item list-group-item-action link"
                     key={allItems[item].name}
-                    onClick={() =>
-                      this.handleClick (
-                        allItems[item]['model ID'],
-                        allItems[item].name,
-                        allItems[item].source
-                      )}
+                    onClick={() => this.handleClick(
+                      allItems[item]['model ID'],
+                      allItems[item].name,
+                      allItems[item].source,
+                    )}
                   >
                     {allItems[item].name}
                   </Link>
