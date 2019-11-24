bacbff173878d7abf8f0c18bf9ec77c10e625132
Denys Fenchenko <denisfench@gmail.com>
2019-11-24 17:16:15 -0500

    Adding borderless option to Home menu

--- webapp/src/components/Home.js
+++ webapp/src/components/Home.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { Loader, Dimmer } from 'semantic-ui-react';
+import React, {Component} from 'react';
+import {Loader, Dimmer} from 'semantic-ui-react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Carousel from './Carousel';
 import sandpileImg from './images/Sandpile.jpg';
 import sandpile1Img from './images/sandpile_2.png';
@@ -12,35 +12,40 @@ import mandelobrotImg from './images/mendelobrot_sq.jpg';
 import './styles.css';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super (props);
     this.state = {
       allItems: [],
       loadingData: false,
       apiFailed: false,
       dataForCarousel: [
-        { image: sandpileImg, title: 'by Seth Terashima' },
-        { image: sandpile1Img, title: 'by Colt Browninga' },
-        { image: mandelobrotImg, title: 'by Adam majewski' },
+        {image: sandpileImg, title: 'by Seth Terashima'},
+        {image: sandpile1Img, title: 'by Colt Browninga'},
+        {image: mandelobrotImg, title: 'by Adam majewski'},
       ],
     };
     this.api_server = 'https://indrasnet.pythonanywhere.com/';
   }
 
-  async componentDidMount() {
+  async componentDidMount () {
     try {
-      this.setState({ loadingData: true });
+      this.setState ({loadingData: true});
       document.title = 'Indra | Home';
-      const res = await axios.get(`${this.api_server}models`);
-      this.setState({ allItems: res.data, loadingData: false });
+      const res = await axios.get (`${this.api_server}models`);
+      this.setState ({allItems: res.data, loadingData: false});
     } catch (e) {
-      this.setState({ apiFailed: true });
+      this.setState ({apiFailed: true});
     }
   }
 
   renderShowDescription = () => (
     <h1 className="small-header">
-      <a href="https://gcallah.github.io/indras_net/index.html" className="text-primary m-2" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://gcallah.github.io/indras_net/index.html"
+        className="text-primary m-2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         View Project Description
         {' '}
       </a>
@@ -49,12 +54,14 @@ class Home extends Component {
   );
 
   handleClick = (id, name, source) => {
-    localStorage.setItem('menu_id', id);
-    localStorage.setItem('name', name);
-    localStorage.setItem('source', source);
-  }
+    localStorage.setItem ('menu_id', id);
+    localStorage.setItem ('name', name);
+    localStorage.setItem ('source', source);
+  };
 
-  renderHeader = () => <h1 className="text-center">Indra Agent-Based Modeling System</h1>;
+  renderHeader = () => (
+    <h1 className="text-center">Indra Agent-Based Modeling System</h1>
+  );
 
   renderChooseModelProp = () => (
     <h1 className="small-header">
@@ -63,14 +70,10 @@ class Home extends Component {
     </h1>
   );
 
-  render() {
-    const {
-      loadingData, dataForCarousel, allItems, apiFailed,
-    } = this.state;
+  render () {
+    const {loadingData, dataForCarousel, allItems, apiFailed} = this.state;
     if (apiFailed) {
-      return (
-        <h1>404 Error</h1>
-      );
+      return <h1>404 Error</h1>;
     }
     if (loadingData) {
       return (
@@ -82,41 +85,40 @@ class Home extends Component {
     return (
       <div className="container">
         <h1 className="margin-top-60"> </h1>
-        <div className="margin-bottom-100">{this.renderHeader()}</div>
+        <div className="margin-bottom-100">{this.renderHeader ()}</div>
         <div className="row">
           <div className="col-6">
-            {this.renderChooseModelProp()}
+            {this.renderChooseModelProp ()}
             <ListGroup>
-              {Object.keys(allItems).map((item) => (
+              {Object.keys (allItems).map (item => (
                 <OverlayTrigger
                   key={`${allItems[item].name}-tooltip`}
                   placement="right"
-                  overlay={(
+                  overlay={
                     <Tooltip>
                       {allItems[item].doc}
                     </Tooltip>
-                  )}
+                  }
                 >
                   <Link
                     to={{
-                      pathname: `/models/props/${
-                        allItems[item]['model ID']
-                      }`,
+                      pathname: `/models/props/${allItems[item]['model ID']}`,
                     }}
-                    className="text-primary w-75 p-3 list-group-item list-group-item-action"
+                    className="text-primary w-75 p-3 list-group-item list-group-item-action borderless"
                     key={allItems[item].name}
-                    onClick={() => this.handleClick(
-                      allItems[item]['model ID'],
-                      allItems[item].name,
-                      allItems[item].source,
-                    )}
+                    onClick={() =>
+                      this.handleClick (
+                        allItems[item]['model ID'],
+                        allItems[item].name,
+                        allItems[item].source
+                      )}
                   >
                     {allItems[item].name}
                   </Link>
                 </OverlayTrigger>
               ))}
             </ListGroup>
-            {this.renderShowDescription()}
+            {this.renderShowDescription ()}
           </div>
           <div className="col-6">
             <Carousel
