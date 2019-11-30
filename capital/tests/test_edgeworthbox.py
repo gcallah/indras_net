614430678ec1fa783e7f852f11122b0a18c4d08f
Ene <lmxyngnl@gmail.com>
2019-11-29 22:28:00 -0500

    added test_create_wagent and test_create_wagent in test_edgeworthbox.py

--- capital/tests/test_edgeworthbox.py
+++ capital/tests/test_edgeworthbox.py
@@ -18,5 +18,40 @@ class EdgeworthboxTestCase(TestCase):
         util = edge.gen_util_func(0)
         self.assertEqual(util, edge.DEF_MAX_UTIL)
 
+    def test_trade(self):
+        pass
+
+
+    def test_rec_offer(self):
+        pass
+
+
+    def test_utility_delta(self):
+        pass
+
+
+    def test_adj_add_good(self):
+        pass
+
+
+    def test_create_wagent(self):
+        agent = edge.create_cagent('Wine holders', 0)
+        name = agent.name
+        camt = agent["goods"]["cheese"]["endow"]
+        wamt = agent["goods"]["wine"]["endow"]
+        self.assertEqual(name, "Wine holders0")
+        self.assertEqual(camt, edge.DEF_NUM_CHEESE)
+        self.assertEqual(wamt, 0)
+
+
+    def test_create_cagent(self):
+        agent = edge.create_cagent('Cheese holders',0)
+        name = agent.name
+        camt = agent["goods"]["cheese"]["endow"]
+        wamt = agent["goods"]["wine"]["endow"]
+        self.assertEqual(name, "Cheese holders0")
+        self.assertEqual(camt, edge.DEF_NUM_CHEESE)
+        self.assertEqual(wamt, 0)
+
     if __name__ == '__main__':
         main()
