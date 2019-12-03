6b78f7ff9b9702d4b195d89736f382e22b2f237b
Carroll Shen <carrolshen@nyu.edu>
2019-12-03 04:14:56 -0500

    Added a test case for flocking

--- models/tests/test_flocking.py
+++ models/tests/test_flocking.py
@@ -6,8 +6,10 @@ from unittest import TestCase, main
 
 from propargs.propargs import PropArgs
 
-from models.flocking import set_up
+from models.flocking import set_up, create_bird
+from models.flocking import COMP_BIRD_NAME
 
+TEST_BNAME = "Birds"
 
 class FlockingTestCase(TestCase):
     def setUp(self):
@@ -17,6 +19,14 @@ class FlockingTestCase(TestCase):
 
     def tearDown(self):
         (self.the_sky, self.flock) = (None, None)
+
+    def test_create_bird(self):
+        """
+        Test to see if bird is created
+        """
+        new_bird = create_bird(TEST_BNAME, 1, props=self.pa)
+        self.assertEqual(new_bird.name, COMP_BIRD_NAME + str(1))
+
 
     if __name__ == '__main__':
         main()
