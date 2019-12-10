567ba732e5fef9dc9d53ca3fdc854a8cdeb9d95b
josephjdkim <josephjdkim@gmail.com>
2019-12-10 13:32:00 -0500

    removed unused Link import to fix failing build

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    // <div className="header">
-    //   <ul className="header-items-list">
-    //     <li className="header-item indra-name">
-    //       <Link to="/">
-    //         <p>INDRA</p>
-    //       </Link>
-    //     </li>
-    //     <li className="header-item">
-    //       <a href="https://gcallah.github.io/indras_net/index.html" target="_blank" rel="noopener noreferrer">
-    //         <p>ABOUT</p>
-    //       </a>
-    //     </li>
-    //     <li className="header-item">
-    //       <a href="https://github.com/gcallah/indras_net/" target="_blank" rel="noopener noreferrer">
-    //         <p>SOURCE CODE</p>
-    //       </a>
-    //     </li>
-    //   </ul>
-    // </div>
-
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="/">INDRA</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
