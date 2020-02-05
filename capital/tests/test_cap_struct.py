b2a6faeaf46ecb53de433af6caf7538643c2e67b
Ene <lmxyngnl@gmail.com>
2020-02-05 01:03:44 -0500

    added test for cap_struct

--- capital/tests/test_cap_struct.py
+++ capital/tests/test_cap_struct.py
@@ -8,15 +8,31 @@ import capital.cap_struct as cap
 
 
 class CapitalTestCase(TestCase):
-    def setUp(self):
-        pass
+    def setUp(self, props=None):
+        self.pa = cap.get_props("cap", props, model_dir="capital")
+        (cap.resource_holders, cap.entrepreneurs) = cap.set_up()
 
     def tearDown(self):
-        pass
+        (cap.resource_holders, cap.entrepreneurs) = (None, None)
+
+    def test_dict_to_string(self):
+        dict = {"cat":3, "dog":4}
+        dict_str = cap.dict_to_string(dict)
+        self.assertEqual(dict_str,"cat 3.00 dog 4.00")
 
     def test_create_entr(self):
         entr = cap.create_entr("Manxueying", 1)
         self.assertEqual(entr.name, "Manxueying1")
+
+    def test_create_rholder(self):
+        rholder = cap.create_rholder("Peiwen", 1)
+        self.assertEqual(rholder.name, "Peiwen1")
+
+    def test_rholder_action(self):
+        pass
+
+    def test_entr_action(self):
+        pass
 
     if __name__ == '__main__':
         main()
--- capital/tests/test_trade.py
+++ capital/tests/test_trade.py
@@ -9,11 +9,27 @@ import capital.trade as td
 
 class tradeTestCase(TestCase):
     def setUp(self, props=None):
+        # self.pa = td.get_props("trade", props, model_dir="capital")
+        # (td.trader_group, td.max_utility) = td.set_up()
         pass
-
 
     def tearDown(self):
+        # (td.trader_group, td.max_utility) = (None, None)
         pass
+
+    def test_random_generate_resources(self):
+        pass
+
+    def test_create_trader(self):
+        pass
+        # waiting for random generate resources being done
+        # agent = td.create_trader('trader', 0)
+        # name = agent.name
+        # pamt = agent["goods"]["penguin"]["endow"]
+        # camt = agent["goods"]["cat"]["endow"]
+        # bamt = agent["goods"]["bear"]["endow"]
+        # famt = agent["goods"]["pet food"]["endow"]
+        # self.assertEqual(name, "trader0")
 
     if __name__ == '__main__':
         main()
