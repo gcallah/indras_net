83fd80928463ac520f0329fd7976769148ea01cb
Ene <lmxyngnl@gmail.com>
2020-02-06 00:23:57 -0500

    added test for cap_struct

--- capital/tests/test_cap_struct.py
+++ capital/tests/test_cap_struct.py
@@ -23,12 +23,27 @@ class CapitalTestCase(TestCase):
     def test_create_entr(self):
         entr = cap.create_entr("Manxueying", 1)
         self.assertEqual(entr.name, "Manxueying1")
+        cash = entr["cash"]
+        wants = entr["wants"]
+        have = entr["have"]
+        self.assertEqual(entr.name, "Manxueying1")
+        self.assertEqual(cash, cap.DEF_ENTR_CASH)
+        self.assertEqual(wants, cap.DEF_CAP_WANTED)
+        self.assertEqual(have, {})
 
     def test_create_rholder(self):
         rholder = cap.create_rholder("Peiwen", 1)
         self.assertEqual(rholder.name, "Peiwen1")
+        cash = rholder["cash"]
+        r = rholder["resources"]
+        price = rholder["price"]
+        self.assertEqual(rholder.name, "Peiwen1")
+        self.assertEqual(cash, 0)
+        self.assertEqual(r, cap.DEF_RESOURCE_HOLD)
+        self.assertEqual(price, cap.DEF_EACH_CAP_PRICE)
 
     def test_rholder_action(self):
+        # rholder is not doing anything
         pass
 
     def test_entr_action(self):
