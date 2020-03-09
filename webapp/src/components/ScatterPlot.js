5a93815a6aa38b0434850c2a28d92fd9e1763607
Abhilash Kulkarni <abhilashkulkarni@Abhilashs-MBP.fios-router.home>
2020-03-09 19:37:23 -0400

    Fix white page error that occurs when setting the parameters

--- webapp/src/components/ScatterPlot.js
+++ webapp/src/components/ScatterPlot.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { ScatterChart } from 'react-chartkick';
-import 'chart.js';
-import PropTypes from 'prop-types';
+import React from "react";
+import { ScatterChart } from "react-chartkick";
+import "chart.js";
+import PropTypes from "prop-types";
 
 const FMARKET = 10;
 function ScatterPlot(props) {
@@ -10,30 +10,24 @@ function ScatterPlot(props) {
   if (loadingData && id !== FMARKET) {
     const env = envFile.members;
     // TODO: width and height are set but never used
-    const WIDTH = envFile.props.grid_height.val;
-    const HEIGHT = envFile.props.grid_width.val;
+    // const WIDTH = envFile.props.grid_height.val;
+    // const HEIGHT = envFile.props.grid_width.val;
     const data = [];
     Object.keys(env).forEach((group, iGroup) => {
       data.push({
         name: env[group].name,
         color: env[group].attrs.color,
-        data: [],
+        data: []
       });
-      Object.keys(env[group].members).forEach((member) => {
+      Object.keys(env[group].members).forEach(member => {
         if (env[group].members[member].pos !== null) {
-          data[iGroup].data.push(
-            env[group].members[member].pos,
-          );
+          data[iGroup].data.push(env[group].members[member].pos);
         }
       });
     });
     return (
       <div>
-        <ScatterChart
-          data={data}
-          width="600px"
-          height="600px"
-        />
+        <ScatterChart data={data} width="600px" height="600px" />
       </div>
     );
   }
@@ -46,13 +40,13 @@ function ScatterPlot(props) {
 ScatterPlot.propTypes = {
   loadingData: PropTypes.bool,
   envFile: PropTypes.shape(),
-  id: PropTypes.number,
+  id: PropTypes.number
 };
 
 ScatterPlot.defaultProps = {
   loadingData: true,
   envFile: {},
-  id: 0,
+  id: 0
 };
 
 export default ScatterPlot;
