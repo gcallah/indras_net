c9afe67e6949031def57ff12ce93abc55f306cbe
Abhilash Kulkarni <abhilashkulkarni@Abhilashs-MBP.fios-router.home>
2020-03-09 19:33:22 -0400

    Updates

---
+++ precache-manifest.3eb5f57b10ad4a4975375b519cab1158.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1187242ebdcb6105726bd92132f9926e",
+    "url": "/indras_net/index.html"
+  },
+  {
+    "revision": "0a69d1dfdbf46b681938",
+    "url": "/indras_net/static/css/2.1056786b.chunk.css"
+  },
+  {
+    "revision": "1f026f05f846d244e3f8",
+    "url": "/indras_net/static/css/main.db17fb3d.chunk.css"
+  },
+  {
+    "revision": "0a69d1dfdbf46b681938",
+    "url": "/indras_net/static/js/2.a4e899d6.chunk.js"
+  },
+  {
+    "revision": "1f026f05f846d244e3f8",
+    "url": "/indras_net/static/js/main.b4b1ef70.chunk.js"
+  },
+  {
+    "revision": "a51c6d74b5c00dece6de",
+    "url": "/indras_net/static/js/runtime-main.f17cb719.js"
+  },
+  {
+    "revision": "090bc1a004cc85c7a9432489536ea04d",
+    "url": "/indras_net/static/media/Sandpile.090bc1a0.jpg"
+  },
+  {
+    "revision": "875dd8b84ac85ca1bc528cc62f140231",
+    "url": "/indras_net/static/media/mendelobrot_sq.875dd8b8.jpg"
+  },
+  {
+    "revision": "d38b3fb15132329ad50c0ece9add51e7",
+    "url": "/indras_net/static/media/sandpile_2.d38b3fb1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/indras_net/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/indras_net/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/indras_net/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/indras_net/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/indras_net/precache-manifest.d134d7ac37e393e973b70deda4384d77.js"
+  "/indras_net/precache-manifest.3eb5f57b10ad4a4975375b519cab1158.js"
 );
 
 self.addEventListener('message', (event) => {
