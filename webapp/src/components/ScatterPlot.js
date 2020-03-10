4c2f957f64dcc84879398d613cf48a3f11a84b1a
Abhilash Kulkarni <abhilashkulkarni@Abhilashs-MBP.fios-router.home>
2020-03-10 10:49:29 -0400

    Fix ESlint errors

--- webapp/src/components/ScatterPlot.js
+++ webapp/src/components/ScatterPlot.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { ScatterChart } from "react-chartkick";
-import "chart.js";
-import PropTypes from "prop-types";
+import React from 'react';
+import { ScatterChart } from 'react-chartkick';
+import 'chart.js';
+import PropTypes from 'prop-types';
 
 const FMARKET = 10;
 function ScatterPlot(props) {
@@ -17,17 +17,23 @@ function ScatterPlot(props) {
       data.push({
         name: env[group].name,
         color: env[group].attrs.color,
-        data: []
+        data: [],
       });
-      Object.keys(env[group].members).forEach(member => {
+      Object.keys(env[group].members).forEach((member) => {
         if (env[group].members[member].pos !== null) {
-          data[iGroup].data.push(env[group].members[member].pos);
+          data[iGroup].data.push(
+            env[group].members[member].pos,
+          );
         }
       });
     });
     return (
       <div>
-        <ScatterChart data={data} width="600px" height="600px" />
+        <ScatterChart
+          data={data}
+          width="600px"
+          height="600px"
+        />
       </div>
     );
   }
@@ -40,13 +46,13 @@ function ScatterPlot(props) {
 ScatterPlot.propTypes = {
   loadingData: PropTypes.bool,
   envFile: PropTypes.shape(),
-  id: PropTypes.number
+  id: PropTypes.number,
 };
 
 ScatterPlot.defaultProps = {
   loadingData: true,
   envFile: {},
-  id: 0
+  id: 0,
 };
 
 export default ScatterPlot;
