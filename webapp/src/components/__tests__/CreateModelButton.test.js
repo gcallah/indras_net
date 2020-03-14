2e003c17093a1a379d97bedcb26ca1738f0dfa4a
Abhilash Kulkarni <abhilashkulkarni@Abhilashs-MBP.fios-router.home>
2020-03-14 13:30:03 -0400

    Add test to CreateModelButton

---
+++ webapp/src/components/__tests__/CreateModelButton.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Link, HashRouter as Router } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import CreateModelButton from '../CreateModelButton';
+import { any } from 'prop-types';
+
+describe('<CreateModelButton/>', () => {
+  it('<CreateModelButton /> should render <Router/>', () => {
+    const component = mount(<CreateModelButton />);
+    expect(component.find(Router)).toHaveLength(1);
+  });
+
+  it('<CreateModelButton /> should render <Link/> with correct props', () => {
+    const component = mount(<CreateModelButton />);
+    expect(component.find(Router)).toHaveLength(1);
+    expect(component.find(Link)).toHaveLength(1);
+    const props = component.find(Link).props();
+    expect(props).toEqual({ to: 'modelcreator', children: expect.anything() });
+    expect(component.find(Link)).toHaveLength(1);
+  });
+
+  it('<CreateModelButton /> should render <Button/> with correct props', () => {
+    const component = mount(<CreateModelButton />);
+    expect(component.find(Router)).toHaveLength(1);
+    expect(component.find(Link)).toHaveLength(1);
+    expect(component.find(Button)).toHaveLength(1);
+    const ButtonProps = component.find(Button).props();
+    expect(ButtonProps).toEqual({
+      variant: 'outline-primary',
+      children: 'Create a new model',
+      active: false,
+      disabled: false,
+      type: 'button'
+    });
+  });
+});
