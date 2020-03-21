2e1399f5a9855c1464418bf952813f3e614a9c6a
Ene <lmxyngnl@gmail.com>
2020-03-21 15:59:28 -0800

    added test for if_compliment()

--- capital/tests/test_complimentary.py
+++ capital/tests/test_complimentary.py
@@ -4,9 +4,9 @@ This is the test suite for trade.py.
 
 from unittest import TestCase, main
 
-import capital.trade as td
+import capital.complimentary as cp
 import capital.trade_utils as tu
-# from capital.trade_utils import 
+from capital.complimentary import COMPLIMENTS 
 
 from capital.trade_utils import AMT_AVAILABLE, seek_a_trade
 from capital.trade_utils import gen_util_func, trade, DEF_MAX_UTIL
@@ -15,18 +15,20 @@ from capital.trade_utils import rec_offer, utility_delta, adj_add_good
 
 class tradeTestCase(TestCase):
     def setUp(self, props=None):
-        # self.goodA = {AMT_AVAILABLE: 10}
-        # self.goodB = {AMT_AVAILABLE: 10}
-        # self.trader = {"goods": {}}
-        # self.goods = {"a": self.goodA, "b": self.goodB}
-        pass
+        self.goodA = {AMT_AVAILABLE: 10, COMPLIMENTS: "b"}
+        self.goodB = {AMT_AVAILABLE: 10}
+        self.trader = {"goods": {"a": self.goodA, "b": self.goodB}}
+        self.goods = {"a": self.goodA, "b": self.goodB}
 
     def tearDown(self):
-        # self.goodA = None
-        # self.goodB = None
-        # self.trader = None
-        # self.goods = None
-        pass
+        self.goodA = None
+        self.goodB = None
+        self.trader = None
+        self.goods = None
+
+    def test_if_compliment(self):
+        self.assertTrue(cp.if_compliment(self.trader, "a", "b"))
+        
 
     def test_create_trader(self):
         # agent = td.create_trader('trader', 0)
