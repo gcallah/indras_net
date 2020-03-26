ae1b51cf2ebf4a6117d1a69f97fb86fffb9d390c
Abhilash Kulkarni <abhikul1993@gmail.com>
2020-03-26 10:54:20 -0400

    Add tests to layout component

---
+++ webapp/src/components/__tests__/Layout.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Container } from 'semantic-ui-react';
+import Layout from '../Layout';
+import Header from '../Header';
+
+describe('<Layout/>', () => {
+  it('<Layout /> should render <Container/>', () => {
+    const children = <div className="foo">foo</div>;
+    const component = mount(<Layout>{children}</Layout>);
+    expect(component.find(Container)).toHaveLength(1);
+  });
+
+  it('<Layout /> should render <link/> with correct props', () => {
+    const children = <div className="foo">foo</div>;
+    const component = mount(<Layout>{children}</Layout>);
+    expect(component.find(Container)).toHaveLength(1);
+    expect(component.find('link')).toHaveLength(1);
+    const props = component.find('link').props();
+    expect(props).toEqual({
+      href:
+        '//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css',
+      rel: 'stylesheet',
+    });
+  });
+
+  it('<Layout /> should render <Header/>', () => {
+    const children = <div className="foo">foo</div>;
+    const component = mount(<Layout>{children}</Layout>);
+    expect(component.find(Container)).toHaveLength(1);
+    expect(component.find(Header)).toHaveLength(1);
+  });
+
+  it('<Layout /> should render children', () => {
+    const children = <div className="foo">foo</div>;
+    const component = mount(<Layout>{children}</Layout>);
+    expect(component.find(Container)).toHaveLength(1);
+    expect(component.find(Header)).toHaveLength(1);
+    const foo = component.find('.foo');
+    expect(foo.props()).toEqual({ className: 'foo', children: 'foo' });
+  });
+});
