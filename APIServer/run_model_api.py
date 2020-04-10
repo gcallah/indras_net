9673d32c96eabfbef59cdd7fe32fe6e07fe529b6
gcallah <gcallah@mac.com>
2020-04-09 23:45:58 -0400

    Added set_env_attrs() call to run_model_api.

--- APIServer/run_model_api.py
+++ APIServer/run_model_api.py
@@ -4,6 +4,12 @@ This module restores an env from json and runs it.
 """
 from APIServer.api_utils import json_converter
 from indra.env import Env
+from models.forestfire import MODEL_NAME as FFMODEL_NAME
+from models.forestfire import set_env_attrs as ff_set_env_attrs
+
+env_attrs = {
+    FFMODEL_NAME: ff_set_env_attrs,
+}
 
 
 def run_model_put(payload, run_time):
@@ -12,5 +18,7 @@ def run_model_put(payload, run_time):
     the real env from the payload.
     """
     env = Env(name='temp name', serial_obj=payload)
+    if env.name in env_attrs:
+        env_attrs[env.name]()
     env.runN(periods=run_time)
     return json_converter(env)
