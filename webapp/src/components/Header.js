799a4e6b235afb7ce5d569bd6cb1fd9c523100d0
Abhilash Kulkarni <abhikul1993@gmail.com>
2020-04-13 11:09:38 -0400

    Make home go to right place based on environment

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
+let home = '/';
+if (process.env.NODE_ENV === 'production') {
+  home = 'https://gcallah.github.io/indras_net/webapp.html#/';
+}
+
 function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Nav className="mr-auto">
-        <Nav.Link href="/">HOME</Nav.Link>
+        <Nav.Link href={home}>HOME</Nav.Link>
       </Nav>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -15,7 +20,6 @@ function Header() {
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-
   );
 }
 
