67aa55646068d736cdc718fd9d8b9a3eddd18f5e
gcallah <gcallah@mac.com>
2020-04-17 01:56:04 -0400

    Moving el farol to new env attr scheme.

--- APIServer/run_model_api.py
+++ APIServer/run_model_api.py
@@ -4,10 +4,13 @@ This module restores an env from json and runs it.
 """
 from APIServer.api_utils import json_converter
 from indra.env import Env
+from models.el_farol import MODEL_NAME as EFMODEL_NAME
+from models.el_farol import set_env_attrs as ef_set_env_attrs
 from models.forestfire import MODEL_NAME as FFMODEL_NAME
 from models.forestfire import set_env_attrs as ff_set_env_attrs
 
 env_attrs = {
+    EFMODEL_NAME: ef_set_env_attrs,
     FFMODEL_NAME: ff_set_env_attrs,
 }
 
--- models/el_farol.py
+++ models/el_farol.py
@@ -14,7 +14,7 @@ from indra.display_methods import BLUE, RED
 from indra.env import Env
 from registry.registry import get_env, get_group, get_prop
 from indra.space import DEF_HEIGHT, DEF_WIDTH
-from indra.user import user_tell, run_notice
+from indra.user import user_tell, run_notice, user_log_notif
 from indra.utils import init_props
 
 DEBUG = False
@@ -22,18 +22,16 @@ DEBUG = False
 MODEL_NAME = "el_farol"
 DRINKERS = "At bar"
 NON_DRINKERS = "At home"
+BAR_ATTEND = "Bar attendees"
+ATTENDANCE = "attendance"
+AGENTS_DECIDED = "agents_decided"
+OPT_OCCUPANCY = "opt_occupancy"
 
 DEF_POPULATION = 10
 DEF_OPTIMAL_OCCUPANCY = int(0.6 * DEF_POPULATION)
 MOTIVATION = [0.6] * DEF_POPULATION
 NUM_DRINKERS = DEF_POPULATION // 2
 NUM_NON_DRINKERS = DEF_POPULATION - NUM_DRINKERS
-
-population = 0
-optimal_occupancy = 0
-attendance_record = []
-attendance = 0
-agents_decided = 0
 
 
 def get_decision(agent):
@@ -66,44 +64,34 @@ def discourage(unwanted):
         drinkers[random_drunk]["motivation"] -= 0.05
         discouraged += 1
         unwanted -= 1
-
     return discouraged
 
 
-def get_avg_attend(record):
-    return sum(record) / len(record)
-
-
 def drinker_action(agent):
-    global attendance
-    global attendance_record
-    global agents_decided
-
     drinkers = get_group(DRINKERS)
     non_drinkers = get_group(NON_DRINKERS)
 
     changed = True
     decision = get_decision(agent)
-    agents_decided += 1
+    bar = get_env()
+    bar.attrs[AGENTS_DECIDED] += 1
 
-    if agents_decided == population:
-        attendance_record.append(attendance)
-        if attendance > optimal_occupancy:
-            extras = attendance - optimal_occupancy
+    attendance = bar.get_attr(ATTENDANCE, 0)
+    opt_occupancy = bar.get_attr(OPT_OCCUPANCY)
+    agents_decided = bar.get_attr(AGENTS_DECIDED)
+    if agents_decided == bar.get_attr("population", 0):
+        if attendance > opt_occupancy:
+            extras = attendance - opt_occupancy
             discourage(extras)
-
-        agents_decided = 0
-        attendance = 0
-        user_tell("Avg attendance so far: "
-                  + str(get_avg_attend(attendance_record)))
+        bar.set_attr(AGENTS_DECIDED, 0)
+        bar.set_attr(ATTENDANCE, 0)
 
     if decision:
-        attendance += 1
+        bar.set_attr(ATTENDANCE, attendance + 1)
         if agent.primary_group() == non_drinkers:
             changed = False
             get_env().add_switch(agent, non_drinkers,
                                  drinkers)
-
     else:
         if agent.primary_group() == drinkers:
             changed = False
@@ -130,16 +118,35 @@ def create_non_drinker(name, i, props=None):
                  attrs={"motivation": 0.6})
 
 
+def setup_attendance(pop_hist):
+    """
+    Set up our pop hist object to record exchanges per period.
+    """
+    pop_hist.record_pop(BAR_ATTEND, 0)
+
+
+def attendance(pop_hist):
+    pop_hist.record_pop(BAR_ATTEND,
+                        get_env().attrs[ATTENDANCE])
+
+
+def attendance_report():
+    return("Attendance this period: "
+           + get_env().attrs[ATTENDANCE])
+
+
+def set_env_attrs():
+    user_log_notif("Setting env attrs for " + MODEL_NAME)
+    env = get_env()
+    env.set_attr("pop_hist_func", attendance)
+    env.set_attr("census_func", attendance_report)
+
+
 def set_up(props=None):
     """
     A func to set up run that can also be used by test code.
     """
-    global population
-    global optimal_occupancy
-    global agents_decided
-
     init_props(MODEL_NAME, props)
-    agents_decided = 0
 
     drinkers = Composite(DRINKERS, {"color": RED},
                          member_creator=create_drinker,
@@ -150,14 +157,15 @@ def set_up(props=None):
                              member_creator=create_non_drinker,
                              num_members=get_prop('population',
                                                   DEF_POPULATION) // 2)
-
+    bar = Env("bar",
+              height=get_prop('grid_height', DEF_HEIGHT),
+              width=get_prop('grid_width', DEF_WIDTH),
+              members=[drinkers, non_drinkers],
+              pop_hist_setup=setup_attendance)
     population = len(drinkers) + len(non_drinkers)
-    optimal_occupancy = int(population * 0.6)
-
-    Env("bar",
-        height=get_prop('grid_height', DEF_HEIGHT),
-        width=get_prop('grid_width', DEF_WIDTH),
-        members=[drinkers, non_drinkers])
+    bar.set_attr("population", population)
+    bar.set_attr(OPT_OCCUPANCY, int(population * 0.6))
+    bar.set_attr(AGENTS_DECIDED, 0)
 
 
 def main():
