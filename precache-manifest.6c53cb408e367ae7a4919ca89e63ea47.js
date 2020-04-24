3e8d5e89a8ac94a68979205cb1a420b7cd28afca
Abhilash Kulkarni <abhikul1993@gmail.com>
2020-04-24 14:27:13 -0400

    Updates

---
+++ precache-manifest.6c53cb408e367ae7a4919ca89e63ea47.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1276476d208941e6419849c0eef3e23",
+    "url": "/indras_net/index.html"
+  },
+  {
+    "revision": "4eb7aa0167a842ef6290",
+    "url": "/indras_net/static/css/2.5afa016d.chunk.css"
+  },
+  {
+    "revision": "063c6b8d444c4c63232f",
+    "url": "/indras_net/static/css/main.3704fe5e.chunk.css"
+  },
+  {
+    "revision": "4eb7aa0167a842ef6290",
+    "url": "/indras_net/static/js/2.0340a03c.chunk.js"
+  },
+  {
+    "revision": "52d292671f7cc30e57e65c3c0313e16f",
+    "url": "/indras_net/static/js/2.0340a03c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063c6b8d444c4c63232f",
+    "url": "/indras_net/static/js/main.1694e8cf.chunk.js"
+  },
+  {
+    "revision": "39a991ece2f972a3b1c6",
+    "url": "/indras_net/static/js/runtime-main.9f0c9c69.js"
+  },
+  {
+    "revision": "090bc1a004cc85c7a9432489536ea04d",
+    "url": "/indras_net/static/media/Sandpile.090bc1a0.jpg"
+  },
+  {
+    "revision": "875dd8b84ac85ca1bc528cc62f140231",
+    "url": "/indras_net/static/media/mendelobrot_sq.875dd8b8.jpg"
+  },
+  {
+    "revision": "d38b3fb15132329ad50c0ece9add51e7",
+    "url": "/indras_net/static/media/sandpile_2.d38b3fb1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/indras_net/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/indras_net/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/indras_net/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/indras_net/static/media/slick.f97e3bbf.svg"
+  }
+]);
