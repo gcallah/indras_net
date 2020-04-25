7bc580525778ba0a55cc70df2174124c8d82fdab
gcallah <gcallah@mac.com>
2020-04-25 14:04:19 -0400

    Added money to run_model_api env restore list.

--- APIServer/run_model_api.py
+++ APIServer/run_model_api.py
@@ -8,10 +8,13 @@ from models.el_farol import MODEL_NAME as EFMODEL_NAME
 from models.el_farol import set_env_attrs as ef_set_env_attrs
 from models.forestfire import MODEL_NAME as FFMODEL_NAME
 from models.forestfire import set_env_attrs as ff_set_env_attrs
+from capital.money import MODEL_NAME as MNMODEL_NAME
+from capital.money import set_env_attrs as mn_set_env_attrs
 
 env_attrs = {
     EFMODEL_NAME: ef_set_env_attrs,
     FFMODEL_NAME: ff_set_env_attrs,
+    MNMODEL_NAME: mn_set_env_attrs,
 }
 
 
