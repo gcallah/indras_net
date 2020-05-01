30ecc1f3fc4666caff5ae60c26ca9ae07d09c041
gcallah <gcallah@mac.com>
2020-05-01 14:45:51 -0400

    Took out stray joson file named like python file from models.

--- models/json_gen_test.py
+++
@@ -1,9 +0,0 @@
-"""
-name: Basic,
-run: basic,
-props: models/props/basic.props.json,
-doc: A toy model to test the system.,
-source: https://github.com/gcallah/indras_net/blob/master/models/basic.py,
-graph: scatter,
-active": true
-"""
