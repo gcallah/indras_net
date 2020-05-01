9604b06bc47cb26a947aa8f4f1c584495a2a5da7
Selina Zhang <yz4184@nyu.edu>
2020-05-01 14:33:24 -0400

    "add to ml"

--- ml/buyer_action_s.py
+++ ml/buyer_action_s.py
@@ -1,70 +1,41 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 25 10:22:19 2020
-
-@author: selina
-"""
-
-'''
-Goal:
-    1. training period:
-    - make a list of good dealer and bad dealer
-    - buy from anyone;
-    - add dealer ratings to attribute
-    - agent try to multilinear regression
-    2. see emojis (from Prof); then see rating factor
-    3. Multi regression line for ML
-    4. Bank and sugeron examples doing machine learning
-    on training - then on test set.
-    5. contest period - new set of dealers following the
-    same rules;
-'''
 import random
-#import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import seaborn as sns
-sns.set()
-
 from indra.agent import Agent
 from registry.registry import get_group, get_env
-from ml.dealer_factory import generate_dealer
-#from ml.stradegy import * 
 
 
-DEBUG = True  # turns debugging code on or off
-DEBUG2 = False  # turns deeper debugging code on or off
-
-DEF_NUM_BUYER  = 10
+DEF_NUM_BUYER = 10
 DEF_NUM_SELLER = 10
-
 MIN_CAR_LIFE = .2
 MEDIUM_CAR_LIFE = 5
 MAX_CAR_LIFE = 10
-
 MATURE_BOUND = 100
-
-
 BUYER_GRP = "Buyer_group"
 DEALER_GRP = "Dealer_group"
 strategies = {}
-# should put in jupiter notebook
+
+sns.set()
+
+
 def multi_linear_regression(agent):
     data = pd.DataFrame(agent["purchase_hist"])
-    y = data["car_life"] 
+    y = data["car_life"]
     emojis = list(agent["emoji_experienced"].keys())
     x1 = data[emojis]
-    x=sm.add_constant(x1)
-    results=sm.OLS(y,x).fit()
+    x = sm.add_constant(x1)
+    results = sm.OLS(y, x).fit()
     results.summary()
     print(results.params)
-    
-# different strategies
-strategies = {"s1": {"func": multi_linear_regression} }
-    
+
+
+strategies = {"s1": {"func": multi_linear_regression}}
+
+
 def is_dealer(agent):  # testcase need changes
     return get_group(DEALER_GRP).ismember(agent)
+
 
 def get_car_life(dealer):  # testcase done
     """
@@ -74,6 +45,7 @@ def get_car_life(dealer):  # testcase done
     print("Getting car from dealer", dealer)
     return dealer["avg_car_life"]
 
+
 def is_mature(buyer):  # testcase done
     """
     check if buyer has enough experience
@@ -81,15 +53,17 @@ def is_mature(buyer):  # testcase done
     """
     return buyer["maturity"] > MATURE_BOUND
 
+
 def learning_w_strategy(agent, dealer):
     organize_data(agent)
     print("Investigating, buyer strategy =", agent["strategy"])
     agent["strategy"]["func"](agent)
-    agent["learnt"] = True
-    # may need to self-testing stage too 
-    
+    agent["learnt"] = True  # may need to self-testing stage too
+
+
 def buy_w_experience(agent, dealer):
     pass
+
 
 def organize_data(agent):
     '''Supervised learnig data collection and organization period'''
@@ -110,21 +84,22 @@ def organize_data(agent):
 
 
 def buy_from_dealer(agent, dealer):
-    ''' used for immature buyers. 
+    ''' used for immature buyers.
     Supervised learnig data collection period'''
     agent["has_car"] = True
-    print("I am an immature buyer. I got a car that last ", 
+    print("I am an immature buyer. I got a car that last ",
           str(dealer["avg_car_life"]),
           ", and my dealer's emoji(s) is/are: ",
           str(dealer["emojis"]))
-    curr_purchase = {"car_life" : dealer["avg_car_life"],
-                     "emojis" : dealer["emojis"]}
+    curr_purchase = {"car_life": dealer["avg_car_life"],
+                     "emojis": dealer["emojis"]}
     for emoji in dealer["emojis"]:
         if emoji in agent["emoji_experienced"]:
-            agent["emoji_experienced"][emoji] = len(agent["emoji_experienced"]) + 1 
-            # skip the 0 
+            length = len(agent["emoji_experienced"]) + 1
+            agent["emoji_experienced"][emoji] = length  # skip the 0
     agent["purchase_hist"].append(curr_purchase)
     agent["car_life"] = dealer["avg_car_life"]
+
 
 def buyer_action(agent):  # how to write this testcase
     """
@@ -141,27 +116,19 @@ def buyer_action(agent):  # how to write this testcase
         if my_dealer is None:
             print("No dealers nearby.")
         elif not is_mature(agent):
-            buy_from_dealer(agent, my_dealer)        
+            buy_from_dealer(agent, my_dealer)
         else:
             if(not agent["learnt"]):
                 learning_w_strategy(agent, my_dealer)
             buy_w_experience(agent, my_dealer)
-            
     else:
         # return False means to move
         print("I have a car!")
         agent["car_life"] -= 1
         if agent["car_life"] <= 0:
-            agent["has_car"] = False 
+            agent["has_car"] = False
     return False
 
-
-
-def create_dealer(name, i, props=None):  # testcase done
-    """
-    Create a dealer agent.
-    """
-    return generate_dealer()
 
 def create_buyer(name, i, props=None):  # testcase done
     """
@@ -172,10 +139,8 @@ def create_buyer(name, i, props=None):  # testcase done
                  attrs={"has_car": False,
                         "maturity": 0,
                         "purchase_hist": [],  # list of purchased correlation
-                        "emoji_experienced": {},  # emojis encountered during immature period
+                        "emoji_experienced": {},  # emojis when immature
                         "learnt": False,
                         "strategy": random.choice(list(strategies.keys())),
                         "car_life": 0
                         })
-
-
