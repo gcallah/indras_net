ce023d26ffe834a422e3162224ee0dda2c96be12
Amadou Diallo <aod259@.nyu.edu>
2020-06-14 14:49:07 -0400

    write testing for healhy person

---
+++ epidemics/tests/test_basic_epi.py
@@ -0,0 +1,21 @@
+""" This is a template file for tests.  """ 
+from unittest import TestCase, main
+import epidemics.basic_epi as be
+
+
+class BasicTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    def testHealty(self):
+	    saliou = be.create_person("Saliou",5)
+	    self.assertEqual(be.is_healthy(saliou),1)
+    def test_main(self):
+        self.assertEqual(be.main(), 0)
+
+if __name__ == '__main__':
+     main()
+
+
