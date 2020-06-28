105701ed730dfca7a5d9e63b88d822f7ad23b90d
gcallah <gcallah@mac.com>
2020-06-27 23:08:10 -0400

    Made variable naming in run_dict consistent.

--- registry/run_dict.py
+++ registry/run_dict.py
@@ -2,82 +2,82 @@
 The file to register functions we need to restore at run time.
 """
 from models.bacteria import bacterium_action, toxin_action, nutrient_action
-from models.bacteria import set_up as bactset_up
-from models.bacteria import MODEL_NAME as BAMODEL_NAME
+from models.bacteria import set_up as ba_set_up
+from models.bacteria import MODEL_NAME as BA_MODEL_NAME
 from models.basic import agent_action
-from models.basic import set_up as baset_up
-from models.basic import MODEL_NAME as BSMODEL_NAME
+from models.basic import set_up as bs_set_up
+from models.basic import MODEL_NAME as BS_MODEL_NAME
 from models.coop import babysitter_action, coop_action, central_bank_action
-from models.coop import set_up as coset_up
-from models.coop import MODEL_NAME as COMODEL_NAME
+from models.coop import set_up as co_set_up
+from models.coop import MODEL_NAME as CO_MODEL_NAME
 from models.coop import set_env_attrs as co_set_env_attrs
-from models.el_farol import set_up as efset_up
+from models.el_farol import set_up as ef_set_up
 from models.el_farol import drinker_action
-from models.el_farol import MODEL_NAME as EFMODEL_NAME
+from models.el_farol import MODEL_NAME as EF_MODEL_NAME
 from models.el_farol import set_env_attrs as ef_set_env_attrs
-from models.fashion import set_up as faset_up
+from models.fashion import set_up as fa_set_up
 from models.fashion import follower_action, common_action, tsetter_action
-from models.fashion import MODEL_NAME as FAMODEL_NAME
+from models.fashion import MODEL_NAME as FA_MODEL_NAME
 from models.fmarket import market_maker_action, trend_follower_action
-from models.fmarket import set_up as fmset_up
+from models.fmarket import set_up as fm_set_up
 from models.fmarket import value_investor_action
-from models.fmarket import MODEL_NAME as FMMODEL_NAME
+from models.fmarket import MODEL_NAME as FM_MODEL_NAME
 from models.fmarket import set_env_attrs as fm_set_env_attrs
-from models.forestfire import set_up as ffset_up
+from models.forestfire import set_up as ff_set_up
 from models.forestfire import tree_action
-from models.forestfire import MODEL_NAME as FFMODEL_NAME
+from models.forestfire import MODEL_NAME as FF_MODEL_NAME
 from models.forestfire import set_env_attrs as ff_set_env_attrs
-from models.sandpile import set_up as sandset_up
+from models.sandpile import set_up as sa_set_up
 from models.sandpile import sandpile_action
-from models.sandpile import MODEL_NAME as SAMODEL_NAME
+from models.sandpile import MODEL_NAME as SA_MODEL_NAME
 from models.sandpile import set_env_attrs as sa_set_env_attrs
-from models.segregation import set_up as segset_up
+from models.segregation import set_up as se_set_up
 from models.segregation import seg_agent_action
-from models.segregation import MODEL_NAME as SEMODEL_NAME
-from models.wolfsheep import set_up as wsset_up
+from models.segregation import MODEL_NAME as SE_MODEL_NAME
+from models.wolfsheep import set_up as ws_set_up
 from models.wolfsheep import sheep_action, wolf_action
 from models.wolfsheep import create_sheep, create_wolf
-from models.wolfsheep import MODEL_NAME as WOMODEL_NAME
-from epidemics.epidemic import set_up as epset_up
+from models.wolfsheep import MODEL_NAME as WO_MODEL_NAME
+from epidemics.epidemic import set_up as ep_set_up
 from epidemics.epidemic import people_action
 from epidemics.epidemic import set_env_attrs as ep_set_env_attrs
-from epidemics.epidemic import MODEL_NAME as EPMODEL_NAME
-from capital.money import set_up as mnset_up
+from epidemics.epidemic import MODEL_NAME as EP_MODEL_NAME
+from capital.money import set_up as mn_set_up
 from capital.money import money_trader_action
-from capital.money import MODEL_NAME as MOMODEL_NAME
+from capital.money import MODEL_NAME as MO_MODEL_NAME
 from capital.money import set_env_attrs as mn_set_env_attrs
-from capital.complementary import set_up as cpset_up
-from capital.complementary import MODEL_NAME as CPMODEL_NAME
+from capital.complementary import set_up as cp_set_up
+from capital.complementary import MODEL_NAME as CP_MODEL_NAME
 from capital.complementary import set_env_attrs as cp_set_env_attrs
-from capital.complementary import MODEL_NAME as CMMODEL_NAME
+from capital.complementary import MODEL_NAME as CM_MODEL_NAME
 from capital.trade_utils import seek_a_trade_w_comp
 
 
 env_attrs = {
-    COMODEL_NAME: co_set_env_attrs,
-    CPMODEL_NAME: cp_set_env_attrs,
-    EFMODEL_NAME: ef_set_env_attrs,
-    EPMODEL_NAME: ep_set_env_attrs,
-    FFMODEL_NAME: ff_set_env_attrs,
-    FMMODEL_NAME: fm_set_env_attrs,
-    MOMODEL_NAME: mn_set_env_attrs,
-    SAMODEL_NAME: sa_set_env_attrs,
+    CO_MODEL_NAME: co_set_env_attrs,
+    CP_MODEL_NAME: cp_set_env_attrs,
+    EF_MODEL_NAME: ef_set_env_attrs,
+    EP_MODEL_NAME: ep_set_env_attrs,
+    FF_MODEL_NAME: ff_set_env_attrs,
+    FM_MODEL_NAME: fm_set_env_attrs,
+    MO_MODEL_NAME: mn_set_env_attrs,
+    SA_MODEL_NAME: sa_set_env_attrs,
 }
 
 setup_dict = {
-    BAMODEL_NAME: bactset_up,
-    BSMODEL_NAME: baset_up,
-    CMMODEL_NAME: cpset_up,
-    COMODEL_NAME: coset_up,
-    EFMODEL_NAME: efset_up,
-    EPMODEL_NAME: epset_up,
-    FAMODEL_NAME: faset_up,
-    FMMODEL_NAME: fmset_up,
-    FFMODEL_NAME: ffset_up,
-    MOMODEL_NAME: mnset_up,
-    SAMODEL_NAME: sandset_up,
-    SEMODEL_NAME: segset_up,
-    WOMODEL_NAME: wsset_up,
+    BA_MODEL_NAME: ba_set_up,
+    BS_MODEL_NAME: bs_set_up,
+    CM_MODEL_NAME: cp_set_up,
+    CO_MODEL_NAME: co_set_up,
+    EF_MODEL_NAME: ef_set_up,
+    EP_MODEL_NAME: ep_set_up,
+    FA_MODEL_NAME: fa_set_up,
+    FM_MODEL_NAME: fm_set_up,
+    FF_MODEL_NAME: ff_set_up,
+    MO_MODEL_NAME: mn_set_up,
+    SA_MODEL_NAME: sa_set_up,
+    SE_MODEL_NAME: se_set_up,
+    WO_MODEL_NAME: ws_set_up,
 }
 
 action_dict = {
