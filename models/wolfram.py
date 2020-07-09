2a94abffcdb77129752de80e60c7d87392cd211a
gcallah <gcallah@mac.com>
2020-07-09 15:26:27 -0400

    Made wolfram model work in standard fashion.

--- models/wolfram.py
+++ models/wolfram.py
@@ -4,15 +4,15 @@ Wolfram's cellular automata model.
 
 import ast
 import os
-import random
 
-from indra.agent import Agent, switch
+from indra.agent import Agent, switch, DONT_MOVE
 from indra.composite import Composite
 from indra.display_methods import BLACK, WHITE, SQUARE
 from indra.env import Env
 from indra.space import DEF_WIDTH
 from indra.utils import init_props
-from registry.registry import get_prop, get_env, get_group, set_env_attr, get_env_attr
+from registry.registry import get_prop, get_env, get_group
+from registry.registry import set_env_attr, get_env_attr, get_registration
 
 MODEL_NAME = "wolfram"
 DEBUG = False  # Turns debugging code on or off
@@ -36,7 +36,6 @@ def get_color(group):
         when passed in a group.
     W and B are integer values:- 0 and 1, respectively.
     """
-
     if group == get_group(WHITE):
         return W
     else:
@@ -50,7 +49,9 @@ def get_rule(rule_num):
     Read from a text file that contains all 256 rules.
     """
     rule_str = ""
-    rules_file = os.path.join(os.getenv("INDRA_HOME", "/home/indrasnet/indras_net"), "models", "wolfram_rules.txt")
+    rules_file = os.path.join(os.getenv("INDRA_HOME",
+                                        "/home/indrasnet/indras_net"),
+                              "models", "wolfram_rules.txt")
     with open(rules_file) as rule_line:
         for i, line in enumerate(rule_line):
             if i == rule_num:
@@ -69,73 +70,69 @@ def next_color(rule_dict, left, middle, right):
     return rule_dict[str((left, middle, right))]
 
 
-def get_str_key(x, y):
+def agent_nm_from_xy(x, y):
     return ("(%d,%d)" % (x, y))
 
 
-def set_wolfram_env_attr():
-    curr_row_idx = get_env_attr("curr_row_idx")
+def top_row():
+    return get_env().height - 1
 
 
-def wolfram_action(wolfram_env: Env):
+def gone_far_enough():
+    get_env().user.error_message["run"] = (' '.join([
+        "You have reached the ",
+        "maximum height ",
+        "and cannot run the model ",
+        "for more periods.\n",
+        "Please pick one of the ",
+        "other options."]))
+    get_env().exclude_menu_item("run")
+
+
+def agent_color(x=None, y=None, agent_nm=None):
+    if x is not None:
+        agent_nm = agent_nm_from_xy(x, y)
+    agent = get_registration(agent_nm)
+    return get_color(agent.primary_group())
+
+
+def wolfram_action(wolf_env: Env):
     """
     The action that will be taken every period.
     """
-    curr_row = wolfram_env.get_row_hood(get_env_attr("curr_row_idx"))
-    # if not isinstance(curr_row, Composite):
-    #     curr_row = Composite(name=curr_row["name"], serial_obj=curr_row)
+    row_above_idx = get_env_attr("prev_row_idx")
+    active_row_idx = row_above_idx - 1
 
-    rule_num = get_prop('rule_number', DEF_RULE)
-    rule_dict = get_rule(rule_num)
-    active_row_y = wolfram_env.height - wolfram_env.get_periods() - 1
-    # print("Active row - {}, period -  {}".format(active_row_y, wolfram_env.get_periods()))
-    if curr_row == wolfram_env.get_row_hood(wolfram_env.height - 1):
-        curr_row = wolfram_env.get_row_hood(active_row_y)
-    wolfram_env.user.tell("\nChecking agents in row " + str(active_row_y)
-                          + " against the rule...")
-    if active_row_y < 1:
-        wolfram_env.user.error_message["run"] = (' '.join([
-            "You have exceeded the ",
-            "maximum height ",
-            "and cannot run the model ",
-            "for more periods.\n",
-            "Please pick one of the ",
-            "other options."]))
-        wolfram_env.exclude_menu_item("run")
+    wolf_env.user.tell("\nChecking agents in row " + str(active_row_idx)
+                       + " against the rule...")
+    if active_row_idx < 1:
+        gone_far_enough()
+        return DONT_MOVE
     else:
-        next_row = wolfram_env.get_row_hood(active_row_y - 1)
-        left_color = get_color(
-            curr_row[get_str_key(0, active_row_y)].primary_group())
-        x = 0
+        left_color = agent_color(x=0, y=active_row_idx)
 
-        for agent in curr_row:
-            # print("Processing agent-{}, x-{}".format(agent, x))
+        x = 0
+        active_row = wolf_env.get_row_hood(active_row_idx)
+        row_above = wolf_env.get_row_hood(row_above_idx)
+        for agent_nm in row_above:
             if DEBUG:
-                print("Checking agent at", curr_row[agent])
-            if (x > 0) and (x < wolfram_env.width - 1):
-                middle_color = get_color(curr_row[agent].primary_group())
-                right_color = get_color(
-                    curr_row[get_str_key(x + 1, active_row_y)].primary_group())
-                # print("Primary group - left - {}, middle - {}, right - {}".format(
-                #     curr_row[get_str_key(0, active_row_y)].primary_group(), curr_row[agent].primary_group(),
-                #     curr_row[get_str_key(x + 1, active_row_y)].primary_group()))
+                print("Checking agent at", row_above[agent_nm])
+            if (x > 0) and (x < wolf_env.width - 1):
+                middle_color = agent_color(agent_nm=agent_nm)
+                right_color = agent_color(x=x+1, y=active_row_idx)
                 if DEBUG:
                     print("  Left: %d, middle: %d, right: %d" %
                           (left_color, middle_color, right_color))
-                if next_color(rule_dict, left_color, middle_color,
-                              right_color):
-                    # print("Adding switch for active_row-{} for agent-{}".format(active_row_y, agent))
-                    wolfram_env.add_switch(next_row[
-                                               get_str_key(x,
-                                                           active_row_y - 1)],
-                                           get_group(WHITE), get_group(BLACK))
-                    next_row[get_str_key(x, active_row_y - 1)].set_prim_group(get_group(BLACK))
+                if next_color(get_env_attr("rule_dict"),
+                              left_color, middle_color, right_color):
+                    wolf_env.add_switch(active_row[
+                                        agent_nm_from_xy(x, active_row_idx)],
+                                        get_group(WHITE), get_group(BLACK))
                 left_color = middle_color
             x += 1
-        # curr_row = next_row
 
-    set_env_attr("curr_row_idx", active_row_y - 1)
-    return True
+    set_env_attr("prev_row_idx", active_row_idx)
+    return DONT_MOVE
 
 
 def set_up(props=None):
@@ -146,18 +143,10 @@ def set_up(props=None):
     init_props(MODEL_NAME, props)
 
     width = get_prop('grid_width', DEF_WIDTH)
-
-    rule_num = get_prop('rule_number', DEF_RULE)
-
-    rule_dict = get_rule(rule_num)
-
     height = (width // 2) + (width % 2)
 
-    white = Composite(WHITE, {"color": WHITE})
-
-    black = Composite(BLACK, {"color": BLACK, "marker": SQUARE})
-
-    groups = [get_group(WHITE), get_group(BLACK)]
+    groups = [Composite(WHITE, {"color": WHITE}),
+              Composite(BLACK, {"color": BLACK, "marker": SQUARE})]
 
     for y in range(height):
         for x in range(width):
@@ -171,16 +160,21 @@ def set_up(props=None):
                              "hide_grid_lines": True,
                              "hide_legend": True},
                       random_placing=False)
-    get_env().exclude_menu_item("line_graph")
+
+    rule_num = get_prop('rule_number', DEF_RULE)
+    wolfram_env.set_attr("rule_num", rule_num)
+    wolfram_env.set_attr("rule_dict", get_rule(rule_num))
+    wolfram_env.exclude_menu_item("line_graph")
 
     '''
     This switch needs to happen before the environment is executed.
-    Using add switch doesn't process the switch until after the environment is executed
-    which breaks the model.
+    Using add switch doesn't process the switch until after
+    the environment is executed which breaks the model.
     '''
-    switch(get_env().get_agent_at(width // 2, height - 1).name, WHITE, BLACK)
-    get_env().get_agent_at(width // 2, height - 1).set_prim_group(get_group(BLACK))
-    set_env_attr("curr_row_idx", get_env().height - 1)
+    top_center_agent = wolfram_env.get_agent_at(width // 2, top_row())
+    switch(top_center_agent.name, WHITE, BLACK)
+    # top row is the "previous" because we just processed it
+    set_env_attr("prev_row_idx", top_row())
 
 
 def main():
