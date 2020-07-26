07d9076336a4525905376e7d3342f77051a328da
Abhinav Sharma <sharma.abhinav503@gmail.com>
2020-07-26 16:24:03 -0400

    Removing debugging flag from api.sh

--- APIServer/api.sh
+++ APIServer/api.sh
@@ -2,5 +2,4 @@
 # The user_type env var is needed to make user interactions within
 # Indra behave properly.
 export user_type="api"
-export FLASK_ENV=development
 FLASK_APP=api_endpoints flask run --host=127.0.0.1 --port=8000
