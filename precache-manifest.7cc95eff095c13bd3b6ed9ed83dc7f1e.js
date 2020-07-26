8e5596a9a266e159dd027f9396a57e17d6bdd078
Denys Fenchenko <denisfench@gmail.com>
2020-07-26 15:21:31 -0400

    Updates

---
+++ precache-manifest.7cc95eff095c13bd3b6ed9ed83dc7f1e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5a2710cdc381a3b57915f1eba19464",
+    "url": "/indras_net/index.html"
+  },
+  {
+    "revision": "d894d7de7a9cf6429100",
+    "url": "/indras_net/static/css/2.f085c4f0.chunk.css"
+  },
+  {
+    "revision": "23528bcc12bc1b129d90",
+    "url": "/indras_net/static/css/main.9462842e.chunk.css"
+  },
+  {
+    "revision": "d894d7de7a9cf6429100",
+    "url": "/indras_net/static/js/2.d074f1fe.chunk.js"
+  },
+  {
+    "revision": "23528bcc12bc1b129d90",
+    "url": "/indras_net/static/js/main.02f200ab.chunk.js"
+  },
+  {
+    "revision": "a51c6d74b5c00dece6de",
+    "url": "/indras_net/static/js/runtime-main.f17cb719.js"
+  },
+  {
+    "revision": "090bc1a004cc85c7a9432489536ea04d",
+    "url": "/indras_net/static/media/Sandpile.090bc1a0.jpg"
+  },
+  {
+    "revision": "875dd8b84ac85ca1bc528cc62f140231",
+    "url": "/indras_net/static/media/mendelobrot_sq.875dd8b8.jpg"
+  },
+  {
+    "revision": "d38b3fb15132329ad50c0ece9add51e7",
+    "url": "/indras_net/static/media/sandpile_2.d38b3fb1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/indras_net/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/indras_net/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/indras_net/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/indras_net/static/media/slick.f97e3bbf.svg"
+  }
+]);
