05ed9bf9ce702dcfc31112178216414d4203e797
Amadou Diallo <aod259@.nyu.edu>
2020-08-24 04:29:54 -0400

    added a js file for barGraph

---
+++ webapp/src/components/PopulationBarGraph.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { ColumnChart } from 'react-chartkick';
+import 'chart.js';
+import PropType from 'prop-types';
+import CardWrapper from './CardWrapper';
+
+function PopulationBarGraph(props) {
+  const NUM_COLORS = 7;
+  const colors = ['red', 'green', 'blue', 'black', 'purple', 'magenta', 'orange'];
+  let thisColor = 0;
+  const { loadingData, envFile } = props;
+  if (loadingData) {
+    const data = [];
+    const env = props.envFile.pop_hist.pops;
+    // populate 'data' array with groups from 'pops'
+    // and their respective values
+    Object.keys(env).forEach((group, iGroup) => {
+      data.push({
+        name: group,
+        // color: colors[thisColor % NUM_COLORS],
+        color: envFile.members[group]
+          ? envFile.members[group].attrs.color : colors[thisColor % NUM_COLORS],
+        data: {},
+      });
+      // modify individual 'data' dictionary of each pops
+      // group by copying over value
+      Object.keys(env[group]).forEach((member, iMember) => {
+        data[iGroup].data[member] = env[group][iMember];
+      });
+      thisColor += 1;
+    });
+    return (
+      <CardWrapper title="Population Bar Graph">
+        <ColumnChart data={data} width="600px" height="600px" />
+
+      </CardWrapper>
+    );
+  }
+  return null;
+}
+
+PopulationBarGraph.propTypes = {
+  loadingData: PropType.bool,
+  envFile: PropType.shape(),
+};
+
+PopulationBarGraph.defaultProps = {
+  loadingData: true,
+  envFile: {},
+};
+
+export default PopulationBarGraph;
