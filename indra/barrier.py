df64b96c8395cbe2b121ca54fa80ff628e8a1427
jkjaykang <yjk441@nyu.edu>
2020-10-07 14:06:46 -0400

    added barrier.py file

---
+++ indra/barrier.py
@@ -0,0 +1,16 @@
+"""
+This file defines barrier, a type of composite,
+which blocks agent's movement
+"""
+from indra.composite import Composite
+
+
+class Barrier(Composite):
+    """
+    The barrier class
+    """
+
+    def temp():
+        """
+        Example method
+        """
