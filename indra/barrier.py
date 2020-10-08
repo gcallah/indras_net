f483c448a1c1b140944e22d2d2a771141e6f4bb0
jkjaykang <yjk441@nyu.edu>
2020-10-08 17:46:30 -0400

    update to barrier.py

--- indra/barrier.py
+++ indra/barrier.py
@@ -10,7 +10,25 @@ class Barrier(Composite):
     The barrier class
     """
 
-    def temp():
+    def __init__(self, name, permeable=False):
         """
-        Example method
+        Should include, dimension of the barrier like height and width,
+        or a list of coordinates for the inital barrier?
+        permeable-can agents "jump" over the barrier?
+        """
+
+    def is_blocked(self, coord):
+        """
+        Check if a coordinate is blocked or not
+        """
+
+    def place_barrier(self, coord):
+        """
+        Inserts a barrier agent at the given coordinate if it is
+        not occupied
+        """
+
+    def remove_barrier(self, coord):
+        """
+        Remove a barrier agent at the given coordinate
         """
